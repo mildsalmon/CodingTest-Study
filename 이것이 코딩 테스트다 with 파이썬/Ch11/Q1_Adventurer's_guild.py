4a4066d586ce301f94c2c370b3e2311190a3a979
mildsalmon_local_pc <mildsalmon@gmail.com>
2021-09-04 21:26:18 +0900

    :zap:[EDIT] 그리드 문제 시간복잡도 개선
    - 리스트의 pop(i) 메소드를 append로 변경하여, 전체 시간 복잡도를 O(N^2)에서 O(N)으로 개선시켰다.
    - 다만, 테스트케이스의 크기가 작아서, 소요되는 시간이 똑같았다..

--- 이것이 코딩 테스트다 with 파이썬/Ch11/Q1_Adventurer's_guild.py
+++ 이것이 코딩 테스트다 with 파이썬/Ch11/Q1_Adventurer's_guild.py
@@ -45,8 +45,17 @@
 #         print(sum(trips))
 # print(len(trips))
 
-n = int(input())
-arrays = list(map(int, input().split()))
+import time
+
+start = time.time()
+
+# n = int(input())
+# arrays = list(map(int, input().split()))
+n = 5
+# arrays = [1, 1, 1, 1, 1]
+# arrays = [4, 4, 4, 4, 4]
+arrays = [3, 2, 2, 2, 1]
+
 arrays.sort(reverse=True)
 group = 0
 start = 0
@@ -63,6 +72,9 @@ while arrays:
     i += 1
 print(group)
 
+end = time.time()
+
+print(end-start)
 # 1시간 25분 25초 / pass / timeout /
 # 풀다가 풀다가 내가 만든 테스트 케이스를 통과하지 못해서 답지 코드를 봤다.
 # 그런데 답지 코드도 내가 만든 테스트 케이스에서 틀린 답을 준다.
@@ -75,3 +87,39 @@ print(group)
 # 5 / 4 4 4 4 4 => 1
 # 5 / 1 2 4 3 1 => 2
 # 5 / 2 3 1 2 2 => 2
+
+start = time.time()
+
+# n = int(input())
+# arrays = list(map(int, input().split()))
+n = 5
+# arrays = [1, 1, 1, 1, 1]
+# arrays = [4, 4, 4, 4, 4]
+arrays = [3, 2, 2, 2, 1]
+
+arrays.sort(reverse=True)
+group = 0
+start = 0
+count = 0
+ar = []
+del_array = []
+for i in range(len(arrays)):
+    if start == count:
+        start = arrays[i]
+        ar.append(start)
+        if len(arrays) - len(del_array) >= start:
+            group += 1
+            count = 0
+    del_array.append(arrays[i])
+    count += 1
+
+print(group)
+
+end = time.time()
+
+print(end-start)
+# 시간 복잡도 개선판.
+# 첫 번째 코드는 pop(i)로 인해 시간복잡도가 O(N^2)이 된다. (while문 N * pop(i) N)
+# 따라서 밑에 코드처럼 작성하면 시간복잡도가 O(N)이 된다. (append는 O(1)이므로)
+
+# time 함수로 시간을 찍어보면서 몇 번 돌려봤는데, 테스트케이스의 길이가 작아서 그런지, 소요 시간이 완전히 똑같이 나온다.

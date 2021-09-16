92a5a1c4caca595c460dc50dc4decc7da4b5ca0c
mildsalmon_local_pc <mildsalmon@gmail.com>
2021-09-16 14:02:48 +0900

    :tada:[Add] PS_(이.취.코)_(프로그래머스)_Ch12.구현_기출문제 Q12

---
+++ 1. PS/1. 이것이 코딩 테스트다 with 파이썬/Ch12/Q12_기둥과 보 설치.py
@@ -0,0 +1,49 @@
+def solution(n, build_frame):
+    game_map = [[5] * (n + 1) for _ in range(n + 1)]
+
+    for bf in build_frame:
+        x, y, a, b = bf
+        y = n - y
+        # game_map[y][x]
+        if b == 1:
+            # 설치
+            if a == 0:
+                # 기둥
+                if y == 5 or game_map[y][x] == 0 or game_map[y][x] == 1:
+                    game_map[y][x] = 0
+                    game_map[y - 1][x] = 0
+            elif a == 1:
+                # 보
+                if game_map[y][x] == 0 or game_map[y][x] == 1 or game_map[y][x - 1] == 1:
+                    game_map[y][x] = 1
+                    game_map[y][x + 1] = 1
+        elif b == 0:
+            # 삭제
+            if a == 0:
+                # 기둥
+                if game_map[y-1][x] != 0 or game_map[y-1][x] != 1:
+                    game_map[y][x] = 5
+                    # game_map[y - 1][x] = 5
+            elif a == 1:
+                # 보
+                if game_map[y][x+1] == 0 or (game_map[y][x-1] != 1 and game_map[y][x+1] != 1):
+                    game_map[y][x] = 5
+                    # game_map[y][x + 1] = 5
+
+    print(*game_map, sep='\n')
+    print()
+
+    answer = []
+
+    for row in range(n+1):
+        for column in range(n+1):
+            if game_map[column][5-row] != 5:
+                # print(game_map[(n-row)][column])
+                answer.append([column, 5-row, game_map[column][5-row]])
+
+    print(answer)
+
+    return b
+
+solution(5, [[1,0,0,1],[1,1,1,1],[2,1,0,1],[2,2,1,1],[5,0,0,1],[5,1,0,1],[4,2,1,1],[3,2,1,1]])
+# solution(5, [[0,0,0,1],[2,0,0,1],[4,0,0,1],[0,1,1,1],[1,1,1,1],[2,1,1,1],[3,1,1,1],[2,0,0,0],[1,1,1,0],[2,2,0,1]])

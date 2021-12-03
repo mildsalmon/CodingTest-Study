3d99628c0e066a6cb882ff36322ef97cc3e13943
mildsalmon_local_pc <mildsalmon@gmail.com>
2021-12-04 08:51:52 +0900

    :tada:[Add] PS_(백준)_스터디 문제 풀이 - 골드 5 - 감소하는 수 - 해결 및 주석추가
    1038번. 골드 5
     - 감소하는 수

--- 1. PS/2. 백준/3. 골드/5/1038.py
+++ 1. PS/2. 백준/3. 골드/5/1038.py
@@ -1,35 +1,31 @@
+"""
+Date    : 2021.12.03
+Update  : 2021.12.04
+Source  : 1038.py
+Purpose : 숫자를 문자열이라고 생각했을때, 문자열의 문자들은 바로 앞의 숫자보다 작아야한다.
+Author  : 김학진 (mildsalmon)
+Email   : mildsalmon@gamil.com
+"""
+def decrease_num(save):
+    for i in range(1, 11):
+        for j in combinations(range(9, -1, -1), i):
+            # combinations에서 1개만 뽑을 때, (1, ) 이렇게 괄호가 쳐지는걸 없애기 위함.
+            temp = list(j)
+            temp_to_str = ''.join(map(str, temp))
+
+            save.append(int(temp_to_str))
+
+from itertools import combinations
+
 n = int(input())
 
 dp = []
-i = 0
-for i in range(1023):
-# while True:
-    temp = str(i)
 
-    pre = temp[0]
-    flag = True
+decrease_num(dp)
+dp.sort()
 
-    for j in temp[1:]:
-        if int(pre) > int(j):
-            pre = j
-            flag = True
-        else:
-            flag = False
-            break
-
-    if flag:
-        dp.append(i)
-        i += 1
-    else:
-        i //= 10
-        i *= 10
-        i += 10
-
-    if len(dp) == n + 1:
-        break
-    # print(dp)
-
-if n < 1023:
-    print(dp[-1])
-else:
+# dp의 길이보다 입력된 수가 크다면 -1
+if len(dp) <= n:
     print(-1)
+else:
+    print(dp[n])

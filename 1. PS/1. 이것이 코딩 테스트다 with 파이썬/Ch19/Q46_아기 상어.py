93beb796af43d8dc649930226958afcd746bdcf2
mildsalmon_local_pc <mildsalmon@gmail.com>
2021-12-07 18:16:43 +0900

    :tada:[Add] PS_(백준)_스터디 문제 풀이 - 골드4 - 2차 풀이 - 주석 추가

--- 1. PS/1. 이것이 코딩 테스트다 with 파이썬/Ch19/Q46_아기 상어.py
+++ 1. PS/1. 이것이 코딩 테스트다 with 파이썬/Ch19/Q46_아기 상어.py
@@ -1,18 +1,19 @@
 from collections import deque
 
 class Area:
+    """
+    공간에 대한 클래스
+    """
     def __init__(self, n, space):
         self.n = n
         self.space = space
         self.shark_pos = self.get_shark_pos()
         self.ds = ((0, 1), (0, -1), (1, 0), (-1, 0))
 
-
     def get_minimum_distance(self, shark_size):
         """
         상어가 모든 지점으로 가는 최단거리를 구함
-        :param shark_pos: 상어 현재 위치
-        :param space: 물고기 지도
+        :param shark_size: BabyShark 클래스에 있는 상어 크기
         :return: 최단거리 지도
         """
         x, y = self.shark_pos
@@ -37,6 +38,10 @@ class Area:
         return distance_map
 
     def get_shark_pos(self):
+        """
+        현재 상어 위치를 요청함.
+        :return: 현재 상어 위치
+        """
         for i in range(n):
             for j in range(n):
                 if self.space[i][j] == 9:
@@ -44,23 +49,37 @@ class Area:
         return self.shark_pos
 
     def update_shark_pos(self, x, y, value):
+        """
+        상어 위치를 업데이트함.
+        :param x: 수정할 상어의 x좌표
+        :param y: 수정할 상어의 y좌표
+        :param value: 수정할 내용 / 0은 상어 지우기, 9는 상어 만들기
+        :return:
+        """
         self.space[x][y] = value
 
     def get_space(self):
+        """
+        현재 남은 물고기 지도를 요청함.
+        :return: 물고기 지도
+        """
         return self.space
 
 
 class BabyShark:
+    """
+    상어에 대한 내용을 담은 클래스
+    """
     def __init__(self, eat, size):
         self.eat = eat
         self.size = size
-
 
     def find_fish(self, minimum_distance_map, space):
         """
         상어가 먹을 수 있는 크기의 물고기를 찾는다.
         만약, 동일한 크기의 물고기가 존재한다면, 우선순위를 (1. 위, 2. 왼쪽)로 정하고 물고기를 찾는다.
         :param minimum_distance_map: 상어에서 모든 물고기까지 가는 최단거리 지도
+        :param space: 물고기 지도
         :return:
         """
         n = len(minimum_distance_map)
@@ -84,8 +103,6 @@ class BabyShark:
         """
         상어가 물고기를 먹는 메소드
         먹은 물고기 수가 상어 크기와 같다면, 상어 크기가 1 커진다.
-        :param eat:
-        :param size:
         :return:
         """
         self.eat += 1
@@ -94,9 +111,11 @@ class BabyShark:
             self.eat = 0
             self.size += 1
 
-        return self.eat, self.size
-
     def get_size(self):
+        """
+        현재 상어의 크기를 요청함.
+        :return: 상어의 크기
+        """
         return self.size
 
 if __name__ == "__main__":
@@ -124,7 +143,7 @@ if __name__ == "__main__":
         if fish is not None:
             time += fish[0]
             area.update_shark_pos(fish[1], fish[2], 9)
-            shark_eat, shark_size = baby_shark.eat_shark()
+            baby_shark.eat_shark()
         else:
             break
 

17afd6887cfb62deca89fbcf836bc1bd573fa6c5
mildsalmon_local_pc <mildsalmon@gmail.com>
2021-12-10 00:16:17 +0900

    :tada:[Add] 스터디 문제 풀이 - 부분수열의 합 - 성공 - 남의 코드 공부한것
    세상에는 천재들이 너무 많다.
    
    수의 패턴을 이렇게 파악할수도 있다니..

--- 1. PS/2. 백준/2. 실버/1/14225.py
+++ 1. PS/2. 백준/2. 실버/1/14225.py
@@ -7,32 +7,15 @@ Author  : 김학진 (mildsalmon)
 Email   : mildsalmon@gamil.com
 """
 
-def dfs(answer, array, depth, partial_sum):
-    if depth >= len(array):
-        return
-
-    partial_sum += array[depth]
-    answer.append(partial_sum)
-
-    dfs(answer, array, depth+1, partial_sum-array[depth])
-    dfs(answer, array, depth+1, partial_sum)
-
-def check(answer):
-    # set으로 강제형변환을 for문 안에서 반복해주면 시간복잡도가 오래걸린다.
-    # 대충 생각했을때는 얼마 안걸리겠지 싶었는데, 이것때문에 시간초과가 발생했다.
-    max_value = max(answer)
-    answer = set(answer)
-    for i in range(1, max_value+1):
-        if i not in answer:
-            return i
-    return max_value+1
-
 n = int(input())
 array = list(map(int, input().split()))
+array.sort()
 
-answer = []
-# array.sort()
-dfs(answer, array, 0, 0)
-# print(answer)
+num = 1
 
-print(check(answer))
+for i in array:
+    if num < i:
+        break
+    num += i
+
+print(num)

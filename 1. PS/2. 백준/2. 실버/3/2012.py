17d393ffb8e5c2421e5c12b5c727bdbaa531249d
mildsalmon_local_pc <mildsalmon@gmail.com>
2021-12-14 11:32:14 +0900

    :zap:[Edit] 스터디 문제 풀이 - 등수 매기기 - 4차 시도(성공) - 다른 사람의 풀이
     계수 정렬이 아니면 어떤 방식으로 풀어야 하나? 라는 생각에 검색해본 다른 사람의 풀이다.
    
     처음에 이 아이디어는 나왔는데, 너무 많이 계산하는거 아니야? 하고 계수정렬에 꽂혔다..

--- 1. PS/2. 백준/2. 실버/3/2012.py
+++ 1. PS/2. 백준/2. 실버/3/2012.py
@@ -6,46 +6,17 @@ Purpose : 계수정렬과 그리디를 이용하여 해결하였다.
 Author  : 김학진 (mildsalmon)
 Email   : mildsalmon@gamil.com
 """
-def rank(counting_sort):
-    """
-    인덱스 에러 해결
-    :param counting_sort:
-    :return:
-    """
-    global n
+n = int(input())
+array = []
 
-    real_rank = []
-    predict_rank = []
-    answer = 0
+for i in range(n):
+    array.append(int(input()))
 
-    for i in range(1, len(counting_sort)):
-        # if counting_sort[i] != 1:
-        if counting_sort[i] >= 1 and i > n:
-            predict_rank.extend([i] * counting_sort[i])
-        elif counting_sort[i] >= 2:
-            predict_rank.extend([i] * (counting_sort[i] - 1))
-        elif counting_sort[i] == 0:
-            real_rank.append(i)
+array.sort()
+answer = 0
 
-    for i in range(len(predict_rank)):
-        temp = predict_rank[i] - real_rank[i]
-        answer += abs(temp)
+for i in range(1, n+1):
+    if i != array[i-1]:
+        answer += abs(array[i-1] - i)
 
-    return answer
-
-if __name__ == "__main__":
-    n = int(input())
-    counting_sort = [-1] * (500001)
-
-    array = []
-
-    for i in range(1, n+1):
-        temp = int(input())
-        array.append(temp)
-        counting_sort[temp] = 0
-        counting_sort[i] = 0
-
-    for a in array:
-        counting_sort[a] += 1
-
-    print(rank(counting_sort))
+print(answer)

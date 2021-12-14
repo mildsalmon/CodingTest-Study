2164fdffbb9389dae44e39369e057f32c3d38dae
mildsalmon_local_pc <mildsalmon@gmail.com>
2021-12-14 11:27:29 +0900

    :zap:[Edit] 스터디 문제 풀이 - 등수 매기기 - 2차 실패 - 계수 정렬로 풀었으나 실패
    이 문제는 계수 정렬로 풀려야한다. 그러나 풀리지 않았다. 그 이유는 다음 코드에서 나온다..

--- 1. PS/2. 백준/2. 실버/3/2012.py
+++ 1. PS/2. 백준/2. 실버/3/2012.py
@@ -1,26 +1,35 @@
 """
-Date    : 2021.12.09
-Update  : 2021.12.13
-Source  : Q46_아기 상어.py
-Purpose : dfs로 구현한 것을 클래스로 변경함.
+Date    : 2021.12.14
+Update  : 2021.12.14
+Source  : 등수 매기기.py
+Purpose : 계수정렬과 그리디를 이용하여 해결하였다.
 Author  : 김학진 (mildsalmon)
 Email   : mildsalmon@gamil.com
 """
-def rank():
+def rank(counting_sort):
+    """
+    인덱스 에러
+    :param counting_sort:
+    :return:
+    """
+    real_rank = []
+    predict_rank = []
     answer = 0
+    count = 0
 
     for i in range(1, len(counting_sort)):
         if counting_sort[i] != 1:
             if counting_sort[i] >= 2:
-                # 현재 순위 * 순위변경이 필요한 인원 수
-                answer += i * (counting_sort[i] - 1)
+                predict_rank.extend([i] * (counting_sort[i] - 1))
             elif counting_sort[i] == 0:
-                answer -= i
+                real_rank.append(i)
+                count += 1
 
-    if answer < 0:
-        return -answer
-    elif answer >= 0:
-        return answer
+    for i in range(count):
+        temp = predict_rank[i] - real_rank[i]
+        answer += abs(temp)
+
+    return answer
 
 if __name__ == "__main__":
     n = int(input())
@@ -30,4 +39,4 @@ if __name__ == "__main__":
         temp = int(input())
         counting_sort[temp] += 1
 
-    print(rank())
+    print(rank(counting_sort))

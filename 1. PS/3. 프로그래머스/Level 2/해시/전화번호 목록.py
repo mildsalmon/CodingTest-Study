de9e573eb0aa7c5f3fa809d5340c02dbc81f0373
mildsalmon_local_pc <mildsalmon@gmail.com>
2021-12-16 10:29:50 +0900

    :zap:[Edit] 전화번호 목록 - 문자열 비교 - 다른 사람의 풀이 추가
    zip()을 이용하면 인자값의 최소길이만큼만 리스트로 반환되는구나..
    
    zip(phoneBook, phoneBook[1:])이면 phoneBook[1:] 길이만큼만 생성된다.
    
    그래서 현재 인덱스와 다음 인덱스를 동시에 p1, p2로 받을 수 있다.
    
    그리고 문자열의 startswitch()를 사용하면 더 쉽게 비교할 수 있구나..

--- 1. PS/3. 프로그래머스/Level 2/해시/전화번호 목록.py
+++ 1. PS/3. 프로그래머스/Level 2/해시/전화번호 목록.py
@@ -22,3 +22,10 @@ def solution(phone_book):
             return False
 
     return True
+
+# def solution(phoneBook):
+#     phoneBook = sorted(phoneBook)
+#     for p1, p2 in zip(phoneBook, phoneBook[1:]):
+#         if p2.startswith(p1):
+#             return False
+#     return True

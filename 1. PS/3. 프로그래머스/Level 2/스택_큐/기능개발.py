a77aed575f98b94f73390732573d36194fc519a7
mildsalmon_local_pc <mildsalmon@gmail.com>
2021-12-17 11:33:09 +0900

    :tada:[Add] 기능개발 - 1차 시도(성공) - 예전에 푼 문제
    신기하게 풀었네..

--- 1. PS/3. 프로그래머스/Level 2/스택_큐/기능개발.py
+++ 1. PS/3. 프로그래머스/Level 2/스택_큐/기능개발.py
@@ -1,3 +1,13 @@
+"""
+Date    : 2021.11.07
+Update  : 2021.12.17
+Source  : 기능개발.py
+Purpose : 스택과 큐를 이용한 문제
+url     : https://programmers.co.kr/learn/courses/30/lessons/42586
+Author  : 김학진 (mildsalmon)
+Email   : mildsalmon@gamil.com
+"""
+
 from collections import deque
 import math
 
@@ -23,3 +33,20 @@ def solution(progresses, speeds):
             time = x_time
 
     return answer
+
+# 정확성  테스트
+# 테스트 1 〉	통과 (0.01ms, 10.3MB)
+# 테스트 2 〉	통과 (0.04ms, 10.2MB)
+# 테스트 3 〉	통과 (0.03ms, 10.2MB)
+# 테스트 4 〉	통과 (0.02ms, 10.2MB)
+# 테스트 5 〉	통과 (0.01ms, 10.1MB)
+# 테스트 6 〉	통과 (0.01ms, 10.4MB)
+# 테스트 7 〉	통과 (0.03ms, 10.1MB)
+# 테스트 8 〉	통과 (0.01ms, 10.4MB)
+# 테스트 9 〉	통과 (0.02ms, 10.4MB)
+# 테스트 10 〉	통과 (0.03ms, 10.2MB)
+# 테스트 11 〉	통과 (0.01ms, 10.2MB)
+#
+# 채점 결과
+# 정확성: 100.0
+# 합계: 100.0 / 100.0

ea880a5a13f9187c569efca62c67463b361022f9
mildsalmon_local_pc <mildsalmon@gmail.com>
2021-12-19 17:53:17 +0900

    :zap:[Edit] 배열 합치기 - 2차 시도(성공) - 투 포인터 사용
    생각보다 빠르지 않네.
    
    while의 반복횟수가 sort()보다 많은 것 같다.

--- 1. PS/2. 백준/2. 실버/5/11728.py
+++ 1. PS/2. 백준/2. 실버/5/11728.py
@@ -2,17 +2,36 @@
 Date    : 2021.12.19
 Update  : 2021.12.19
 Source  : 11728.py
-Purpose : 리스트의 extend 함수 사용하여 해결. (속도 느림)
+Purpose : 투포인터 사용
 url : https://www.acmicpc.net/problem/11728
 Author  : 김학진 (mildsalmon)
 Email   : mildsalmon@gamil.com
 """
 n, m = list(map(int, input().split()))
 
+# 두 배열은 정렬되어서 입력됨
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 
-A.extend(B)
-A.sort()
+answer = []
 
-print(*A, sep=' ')
+A_pos = 0
+B_pos = 0
+
+#
+while len(A) > A_pos and len(B) > B_pos:
+    if A[A_pos] >= B[B_pos]:
+        answer.append(B[B_pos])
+        B_pos += 1
+    elif A[A_pos] < B[B_pos]:
+        answer.append(A[A_pos])
+        A_pos += 1
+
+if len(A) != A_pos:
+    # answer.extend(A[A_pos:])
+    print(*answer, sep=' ', end=' ')
+    print(*A[A_pos:], sep=' ')
+elif len(B) != B_pos:
+    # answer.extend(B[B_pos:])
+    print(*answer, sep=' ', end=' ')
+    print(*B[B_pos:], sep=' ')

30e2005c86f48ebf239575562f9c4764ef016189
mildsalmon_local_pc <mildsalmon@gmail.com>
2021-12-30 08:44:02 +0900

    :tada:[Add] 연속합 - 1차 시도 (성공)
    n이 10만인 것을 고려하여 코드를 작성해야한다.

--- 1. PS/2. 백준/2. 실버/2/1912.py
+++ 1. PS/2. 백준/2. 실버/2/1912.py
@@ -1,51 +1,21 @@
+"""
+Date    : 2021.12.30
+Update  : 2021.12.30
+Source  : 1912.py
+Purpose : n이 10만일때의 dp
+url     : https://www.acmicpc.net/problem/1912
+Author  : 김학진 (mildsalmon)
+Email   : mildsalmon@gamil.com
+"""
+import sys
+
+input = sys.stdin.readline
+
 n = int(input())
 array = list(map(int, input().split()))
-answer = -1001
+dp = array[:]
 
-# for i in range(len(array)):
-#     for j in range(0, len(array)):#, i+1):
-#         start = j
-#         end = j + i+1
-#         answer.append(sum(array[start:end]))
-#         # print(answer)
-#
-# print(max(answer))
-
-# A = sorted(array, reverse=True)
-# B = []
-# # print(A)
-# for i in A:
-#     B.append(array.index(i))
-#
-# # print(B)
-# big = B[0]
-# start = 0
-# end = len(B)-1
-# for i in range(len(B)):
-#     if B[i] <= B[0]:
-#         answer = max(answer, (sum(array[B[i]:B[0]+1])))
-#         start = i
-#     else:
-#         answer = max(answer, (sum(array[B[0]:B[i]+1])))
-#         end = i
-#     answer = max(answer, sum(array[B[start]:B[end]+1]))
-#
-# print(answer)
-
-# answer = sum(array)
-#
-# for i in range(len(array)-1):
-#     if array[0] >= array[-1]:
-#         array.pop()
-#     elif array[0] < array[-1]:
-#         array.pop(0)
-#     answer = max(answer, sum(array))
-#
-# print(answer)
-#
-answer = []
-answer.append(array[0])
 for i in range(1, n):
-    answer.append(max(answer[i-1]+array[i], array[i]))
+    dp[i] = max(array[i], dp[i-1] + array[i])
 
-print(max(answer))
+print(max(dp))

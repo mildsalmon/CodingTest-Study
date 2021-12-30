1367ff518ca7031d9c7527c42f374c519d352c87
mildsalmon_local_pc <mildsalmon@gmail.com>
2021-12-30 11:47:04 +0900

    :zap:[Edit] 알고스팟 - 4차 시도 (성공 - bfs추가 및 함수화)
    bfs로도 가능하더라, 그래서 따로 bfs 함수를 추가해봤다.

--- 1. PS/2. 백준/3. 골드/4/1261.py
+++ 1. PS/2. 백준/3. 골드/4/1261.py
@@ -7,43 +7,84 @@ url     : https://www.acmicpc.net/problem/1261
 Author  : 김학진 (mildsalmon)
 Email   : mildsalmon@gamil.com
 """
-import heapq
 
-m, n = list(map(int, input().split()))
-graph = []
+def dijkstra(start, graph) -> int:
+    import heapq
 
-for i in range(n):
-    temp = list(map(int, input()))
-    graph.append(temp)
+    global n, m
 
-ds = ((1,0), (0, 1), (-1, 0), (0, -1))
+    ds = ((1, 0), (0, 1), (-1, 0), (0, -1))
 
-visited = [[False] * m for _ in range(n)]
+    visited = [[False] * m for _ in range(n)]
 
-q = []
-heapq.heappush(q, (graph[0][0], 0, 0))
+    q = []
+    # (벽을 부순 횟수, x, y)
+    heapq.heappush(q, (0, start[0], start[1]))
 
-break_wall_count = 0
+    while q:
+        break_wall, x, y = heapq.heappop(q)
+        # 만약 방문한 위치라면 continue
+        if not visited[x][y]:
+            if x == n - 1 and y == m - 1:
+                break
 
-while q:
-    wall, x, y = heapq.heappop(q)
+            visited[x][y] = True
+            # 상,하,좌,우 4방향 탐색
+            for d in ds:
+                dx = x + d[0]
+                dy = y + d[1]
 
-    if not visited[x][y]:
-        if x == n-1 and y == m-1:
-            break_wall_count = wall
-            break
+                if 0 <= dx < n and 0 <= dy < m:
+                    # 방문하지 않은 위치만 q에 집어넣음
+                    if not visited[dx][dy]:
+                        if graph[dx][dy] == 1:
+                            heapq.heappush(q, (break_wall + 1, dx, dy))
+                        else:
+                            heapq.heappush(q, (break_wall, dx, dy))
 
-        visited[x][y] = True
-        # 상,하,좌,우 4방향 탐색
+    return break_wall
+
+def bfs(start, graph) -> int:
+    from collections import deque
+
+    global n, m
+
+    ds = ((1, 0), (0, 1), (-1, 0), (0, -1))
+
+    distance = [[-1] * m for _ in range(n)]
+    distance[0][0] = 0
+
+    q = deque()
+    q.append((start[0], start[1]))
+
+    while q:
+        x, y = q.popleft()
+
         for d in ds:
             dx = x + d[0]
             dy = y + d[1]
 
             if 0 <= dx < n and 0 <= dy < m:
-                if not visited[dx][dy]:
+                # 방문하지 않은 곳이라면,
+                if distance[dx][dy] == -1:
+                    # 벽이라면,
                     if graph[dx][dy] == 1:
-                        heapq.heappush(q, (wall + 1, dx, dy))
+                        q.append((dx, dy))
+                        distance[dx][dy] = distance[x][y] + 1
+                    # 벽이 아니라면,
                     else:
-                        heapq.heappush(q, (wall, dx, dy))
+                        q.appendleft((dx, dy))
+                        distance[dx][dy] = distance[x][y]
 
-print(break_wall_count)
+    return distance[n-1][m-1]
+
+
+if __name__ == "__main__":
+    m, n = list(map(int, input().split()))
+    graph = []
+
+    for i in range(n):
+        temp = list(map(int, input()))
+        graph.append(temp)
+
+    print(bfs(start=(0,0), graph=graph))

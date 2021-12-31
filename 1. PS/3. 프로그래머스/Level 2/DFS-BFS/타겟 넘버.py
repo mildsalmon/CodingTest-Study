9441395c1fde9d042dd41ed73d6c3746b000b13e
mildsalmon_local_pc <mildsalmon@gmail.com>
2021-12-31 13:51:30 +0900

    :zap:[Edit] 타겟 넘버 - 2차 시도 (다른 사람의 코드)
    모든 원소는 -x, x 두 가지 상태밖에 없다.
    
    따라서 모든 원소의 (-x, x)를 따로 구하고, 해당 값에 대해 cartesian product를 진행하고 합을 구한다.
    
    그리고 target이 리스트에 몇 개 들어있는지 확인한다.

--- 1. PS/3. 프로그래머스/Level 2/DFS-BFS/타겟 넘버.py
+++ 1. PS/3. 프로그래머스/Level 2/DFS-BFS/타겟 넘버.py
@@ -8,34 +8,16 @@ Author  : 김학진 (mildsalmon)
 Email   : mildsalmon@gamil.com
 """
 
-def dfs(depth: int, numbers: list, target: int) -> None:
-    global count
+from itertools import product
 
-    # 종료조건
-    # 합이 target과 같거나 작으면 더 이상 재귀를 수행할 필요가 없으므로 재귀 종료
-    if sum(numbers) < target:
-        return
-    elif sum(numbers) == target:
-        count += 1
-        print(depth, numbers)
-        return
+def solution(numbers, target) -> int:
+    iter: list = [(x, -x) for x in numbers]
+    # product는 db의 cartesian product이다.
+    # 따라서, iter의 모든 원소의 데카르트 곱이 반환된다.
+        # [(1, -1), (2, -2)] 이면, (1, 2), (1, -2), (-1, 2), (-1, -2)이다.
+    answer: list = list(map(sum, product(*iter)))
 
-    # 수평 탐색
-    # 재귀가 아래로 내려가는 거라면, 재귀 안에서는 다음 노드를 수평적으로 탐색함.
-    for i in range(depth, len(numbers)):
-        numbers[i] = -numbers[i]
-        dfs(i + 1, numbers, target)
-        numbers[i] = -numbers[i]
-    return
-
-
-def solution(numbers: list, target: int) -> int:
-    global count
-
-    count: int = 0
-    dfs(0, numbers, target)
-
-    return count
+    return answer.count(target)
 
 if __name__ == "__main__":
     print(solution([1,1,2,2,1], 3))

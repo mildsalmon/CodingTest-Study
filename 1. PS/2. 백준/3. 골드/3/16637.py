333769ea017a3a5d3554595e2cb29e5a8fdcf50b
mildsalmon_local_pc <mildsalmon@gmail.com>
2022-01-02 10:41:58 +0900

    :tada:[Add] 괄호 추가하기 - 1차 시도 (실패)
    분명 조건상 틀릴 수가 없었다.
    
    반례를 생각해보니 정답이 음수인 경우에도 이 코드에서는 0을 반환한다.
    answer의 초기값이 0이기 때문이다.
    
    따라서 초기값 0을 음수 최대값으로 초기화하면 해결될 것 같다.

---
+++ 1. PS/2. 백준/3. 골드/3/16637.py
@@ -0,0 +1,55 @@
+"""
+Date    : 2022.01.02
+Update  : 2022.01.02
+Source  : 16637.py
+Purpose : 재귀 / 완전탐색 / 브루트포스 / 백트래킹 / DFS
+url     : https://www.acmicpc.net/problem/16637
+Author  : 김학진 (mildsalmon)
+Email   : mildsalmon@gamil.com
+"""
+
+def operation(num_1, num_2, oper) -> int:
+    if oper == '+':
+        B = num_1 + num_2
+    elif oper == '*':
+        B = num_1 * num_2
+    elif oper == '-':
+        B = num_1 - num_2
+
+    return B
+
+def recursive(result, i, num, oper) -> None:
+    global answer
+
+    if i >= len(oper):
+        answer = max(answer, result)
+        return
+
+    if i+2 < len(num):
+        B: int = operation(num[i+1], num[i+2], oper[i+1])
+
+        recursive(operation(result, B, oper[i]), i+2, num, oper)
+
+    recursive(operation(result, num[i+1], oper[i]), i+1, num, oper)
+
+if __name__ == "__main__":
+
+    n: int = int(input())
+    num: list = []
+    oper: list = []
+
+    array: list = list(input())
+
+    answer: int = 0
+
+    for i, value in enumerate(array):
+        if i % 2 == 0:
+            num.append(int(value))
+        elif i % 2 == 1:
+            oper.append(value)
+
+    result: int = num[0]
+
+    recursive(result, 0, num, oper)
+
+    print(answer)

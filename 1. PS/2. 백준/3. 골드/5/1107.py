9cd65042f4f7cd50b79d3c7a67a8cf508597332c
mildsalmon_local_pc <mildsalmon@gmail.com>
2022-01-05 11:04:16 +0900

    :zap:[Edit] 리모컨 - 4차 시도 (실패 - 으 모르겠다)
    탐색 범위를 반으로 쪼개봤는데, 예제에서 틀린다.
    
    무작정 탐색 범위를 반으로 쪼개는게 답이 아닌거 같은데, 어떤 방식으로 해결해야할지 모르겠다.

--- 1. PS/2. 백준/3. 골드/5/1107.py
+++ 1. PS/2. 백준/3. 골드/5/1107.py
@@ -10,34 +10,48 @@ Email   : mildsalmon@gamil.com
 
 import sys
 
-input = sys.stdin.readline
+# input = sys.stdin.readline().rstrip()
 
 def move_channel(depth, remote_control, channel):
     global n, min_push_button
 
     # 종료조건
-    if len(str(n)) - 1 <= depth <= len(str(n)) + 1 and len(channel) != 0:
-        min_push_button = min(min_push_button, depth + abs(int(channel) - n))
+    if len(n) - 1 <= depth <= len(n) + 1 and len(channel) != 0:
+        min_push_button = min(min_push_button, depth + abs(int(channel) - int(n)))
 
-        if depth == len(str(n)) + 1:
+        if depth == len(n) + 1:
             return
 
     # 완전탐색
-    for i in range(10):
-        if remote_control[i]:
-            move_channel(depth+1, remote_control, channel + str(i))
+    if depth < len(n) - 1:
+        bound = [i%10 for i in range(int(n[depth]) - 2, int(n[depth]) + 3)]
+        # bound = [int(n[depth]) - 1, int(n[depth]), (int(n[depth]) + 2) % 10]
+
+        for i in bound:
+            if remote_control[i]:
+                move_channel(depth+1, remote_control, channel + str(i))
+    else:
+        for i in range(10):
+            if remote_control[i]:
+                move_channel(depth+1, remote_control, channel + str(i))
 
 
 if __name__ == "__main__":
-    n: int = int(input())
-    m: int = int(input())
-    trouble_button = set(map(int, input().split()))
+    while True:
+        n: str = input()
+        m: int = int(input())
 
-    remote_control = [False if i in trouble_button else True for i in range(10)]
+        if m == 0:
+            trouble_button = set()
+        else:
+            trouble_button = set(map(int, input().split()))
+        # print(n)
+        # print(trouble_button)
+        remote_control = [False if i in trouble_button else True for i in range(10)]
 
-    min_push_button = abs(100 - n)
+        min_push_button = abs(100 - int(n))
 
-    if min_push_button != 0:
-        move_channel(0, remote_control, '')
+        if min_push_button != 0:
+            move_channel(0, remote_control, '')
 
-    print(min_push_button)
+        print(min_push_button)

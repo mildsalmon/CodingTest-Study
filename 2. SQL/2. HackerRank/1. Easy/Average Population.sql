72d0db5d423fe790baa7c48f98ea35e7430e6986
mildsalmon_local_pc <mildsalmon@gmail.com>
2022-01-10 11:48:17 +0900

    :tada:[Add] Average Population - 1차 시도 (성공)

---
+++ 2. SQL/2. HackerRank/1. Easy/Average Population.sql
@@ -0,0 +1,12 @@
+/*
+Date    : 2022.01.10
+Update  : 2022.01.10
+Source  : Average Population.sql
+Purpose : floor / avg
+url     : https://www.hackerrank.com/challenges/revising-aggregations-the-average-function/problem?isFullScreen=true
+Author  : 김학진 (mildsalmon)
+Email   : mildsalmon@gamil.com
+*/
+
+SELECT FLOOR(AVG(population))
+FROM CITY;

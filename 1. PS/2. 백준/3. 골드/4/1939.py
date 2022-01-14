5aa62e55c4cc2efdf6441bed92177cdd6e7d9bdd
mildsalmon_local_pc <mildsalmon@gmail.com>
2022-01-14 11:57:44 +0900

    :zap:[Edit] 중량제한 - 4차 시도 (성공 - 다른 사람 코드)

--- 1. PS/2. 백준/3. 골드/4/1939.py
+++ 1. PS/2. 백준/3. 골드/4/1939.py
@@ -2,69 +2,50 @@
 Date    : 2022.01.14
 Update  : 2022.01.14
 Source  : 1939.py
-Purpose : 이진탐색 / bfs
+Purpose : 이진탐색 / bfs / union-find / heap / sys 속도 체감
 url     : https://www.acmicpc.net/problem/1939
 Author  : 김학진 (mildsalmon)
 Email   : mildsalmon@gamil.com
 """
 
-from collections import deque
+import heapq
+import sys
 
+input = sys.stdin.readline
 
-def bfs(max_root):
-    global src, dest, n, islands
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
 
-    q = deque()
-    q.append(src)
-    visited = [False] * n
-    visited[src] = True
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
 
-    while q:
-        island = q.popleft()
-
-        # 이 부분이 시간 초과를 해결하는 부분이다.
-        if visited[dest]:
-            break
-
-        for next_weight, next_island in islands[island]:
-            if not visited[next_island] and next_weight >= max_root:
-                q.append(next_island)
-                visited[next_island] = True
-
-    return visited[dest]
-
-
-def binary_search(max_root, min_root):
-    result = min_root
-
-    while min_root <= max_root:
-        mid_root = (max_root + min_root) // 2
-
-        check = bfs(mid_root)
-
-        if check:
-            result = mid_root
-            min_root = mid_root + 1
-        else:
-            max_root = mid_root - 1
-
-    return int(result)
-
+    if a > b:
+        parent[a] = b
+    elif a < b:
+        parent[b] = a
 
 if __name__ == "__main__":
     # 섬, 다리
     n, m = list(map(int, input().split()))
 
-    islands = [[] for _ in range(n)]
+    bridges = []
 
     for i in range(m):
         a, b, weight = list(map(int, input().split()))
 
-        islands[a - 1].append((weight, b - 1))
-        islands[b - 1].append((weight, a - 1))
+        heapq.heappush(bridges, (-weight, a-1, b-1))
 
     src, dest = list(map(lambda x: int(x) - 1, input().split()))
 
-    max_root, min_root = 1e9, 1
+    parent = [i for i in range(n)]
 
-    print(binary_search(max_root, min_root))
+    while bridges:
+        current_weight, current_a, current_b = heapq.heappop(bridges)
+
+        union_parent(parent, current_a, current_b)
+        if find_parent(parent, src) == find_parent(parent, dest):
+            print(-current_weight)
+            break

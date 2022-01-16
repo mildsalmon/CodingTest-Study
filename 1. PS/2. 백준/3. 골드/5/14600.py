e60650bf802b2bc32ab6715b42bfeb0b7217fced
mildsalmon_local_pc <mildsalmon@gmail.com>
2022-01-16 18:02:35 +0900

    :zap:[Edit] 샤워실 바닥 깔기 (Small) - 2차 시도 (성공)
    타일 체크를 하면서 안쪽부터 칠하며 밖으로 나가면 해결할 수 있다.

--- 1. PS/2. 백준/3. 골드/5/14600.py
+++ 1. PS/2. 백준/3. 골드/5/14600.py
@@ -8,6 +8,36 @@ Author  : 김학진 (mildsalmon)
 Email   : mildsalmon@gamil.com
 """
 
+def check_tile(size: int, x: int, y: int) -> bool:
+    global tile
+
+    for i in range(x, x+size):
+        for j in range(y, y+size):
+            if tile[i][j] != 0:
+                return False
+    return True
+
+def fill_tile(tile_num: int, tile: list, size: int, x: int, y: int) -> int:
+    tile_num += 1
+    temp_size = size >> 1
+    if check_tile(temp_size, x, y):
+        tile[temp_size + x - 1][temp_size + y - 1] = tile_num
+    if check_tile(temp_size, x, y+temp_size):
+        tile[temp_size + x - 1][temp_size + y] = tile_num
+    if check_tile(temp_size, x+temp_size, y):
+        tile[temp_size + x][temp_size + y - 1] = tile_num
+    if check_tile(temp_size, x+temp_size, y+temp_size):
+        tile[temp_size + x][temp_size + y] = tile_num
+
+    if size == 2:
+        return tile_num
+
+    tile_num = fill_tile(tile_num, tile, temp_size, x, y)
+    tile_num = fill_tile(tile_num, tile, temp_size, x, y+temp_size)
+    tile_num = fill_tile(tile_num, tile, temp_size, x+temp_size, y)
+    tile_num = fill_tile(tile_num, tile, temp_size, x+temp_size, y+temp_size)
+
+    return tile_num
 
 if __name__ == "__main__":
     k = int(input())
@@ -17,5 +47,8 @@ if __name__ == "__main__":
     tile = [[0] * tile_size for _ in range(tile_size)]
     tile[x][y] = -1
 
+    fill_tile(0, tile, tile_size, 0, 0)
 
-    print(*tile[::-1], sep='\n')
+    # print(*tile[::-1], sep='\n')
+    # ' '.join()의 인자값은 iterable 객체이다. 다만, iterable 객체 안의 값들은 문자열들이여야 한다.
+    print(*map(lambda x: ' '.join(map(str, x)), tile[::-1]), sep='\n')

c3f1bc010c021d9d38f92a59f669e288f085df52
mildsalmon_local_pc <mildsalmon@gmail.com>
2022-01-17 11:00:37 +0900

    :tada:[Add] Weather Observation Station 19 - 1차 시도 (실패)
    유클리드 거리에 필요한 power(제곱), sqrt(제곱근) 함수를 까먹었다.

---
+++ 2. SQL/2. HackerRank/2. Medium/Weather Observation Station 19.sql
@@ -0,0 +1,9 @@
+/*
+Date    : 2022.01.17
+Update  : 2022.01.17
+Source  : Weather Observation Station 19.sql
+Purpose : euclidean distance / power / sqrt / round / max / min
+url     : https://www.hackerrank.com/challenges/weather-observation-station-19/problem?isFullScreen=true
+Author  : 김학진 (mildsalmon)
+Email   : mildsalmon@gamil.com
+*/

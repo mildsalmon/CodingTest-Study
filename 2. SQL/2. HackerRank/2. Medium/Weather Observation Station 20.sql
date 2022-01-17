025e06fce3ac112fe4b7bc24116eb6fda0c728ef
mildsalmon_local_pc <mildsalmon@gmail.com>
2022-01-17 11:34:17 +0900

    :zap:[Edit] Weather Observation Station 20 - 3차 시도 (성공)
    다른 dbms에서도 가능한 방식

--- 2. SQL/2. HackerRank/2. Medium/Weather Observation Station 20.sql
+++ 2. SQL/2. HackerRank/2. Medium/Weather Observation Station 20.sql
@@ -2,12 +2,35 @@
 Date    : 2022.01.17
 Update  : 2022.01.17
 Source  : Weather Observation Station 20.sql
-Purpose : 중앙값 / MEDIAN
+Purpose : 중앙값 / MEDIAN / window function / count / row_number / ceil / floor / avg / round
 url     : https://www.hackerrank.com/challenges/weather-observation-station-20/problem?isFullScreen=true
 Author  : 김학진 (mildsalmon)
 Email   : mildsalmon@gamil.com
 */
 
-SELECT ROUND(MEDIAN(lat_n), 4)
-FROM station
+/*
+ 중앙값
+    row의 수가 홀수일때
+        index가 1부터 시작하기 때문에, 총 row의 수 + 1 / 2 = 중앙값의 index
+    row의 수가 짝수일때
+        총 row의 수 + 1 / 2의 올림, 내림 = 중앙값의 index
+
+    ===
+
+    row의 수가 홀수
+        10, 20, 30, 40, 50
+            (5 + 1) / 2 = 3
+    row의 수가 짝수
+        10, 20, 30, 40
+            (4 + 1) / 2의 올림, 내림 = 3, 2
+
+ */
+
+SELECT ROUND(AVG(lat_n), 4)
+FROM (SELECT lat_n,
+        ROW_NUMBER() OVER(ORDER BY lat_n) AS RN,
+        COUNT(1) OVER() + 1 AS CNT
+      FROM station
+     )
+WHERE RN IN (CEIL(CNT/2), FLOOR(CNT/2))
 ;

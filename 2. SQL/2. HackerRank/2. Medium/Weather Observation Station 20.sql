4a1056f31fb16bac7bc635597fdce93e77056827
mildsalmon_local_pc <mildsalmon@gmail.com>
2022-01-17 11:27:37 +0900

    :tada:[Add] Weather Observation Station 20 - 1차 시도 (실패)

---
+++ 2. SQL/2. HackerRank/2. Medium/Weather Observation Station 20.sql
@@ -0,0 +1,10 @@
+/*
+Date    : 2022.01.17
+Update  : 2022.01.17
+Source  : Weather Observation Station 20.sql
+Purpose : 중앙값 / window function / count / row_number / ceil / floor / avg / round
+url     : https://www.hackerrank.com/challenges/weather-observation-station-20/problem?isFullScreen=true
+Author  : 김학진 (mildsalmon)
+Email   : mildsalmon@gamil.com
+*/
+

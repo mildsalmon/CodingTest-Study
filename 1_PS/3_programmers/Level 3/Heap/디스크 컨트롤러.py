6f609280a624be21ea00a8401b91c6d7a47c8750
mildsalmon_local_pc <mildsalmon@gmail.com>
2022-01-21 11:24:35 +0900

    :zap:[Edit] 디스크 컨트롤러 - 4차 시도 (처음에 고려했던 비효율성 개선)
    분명 더 빨라져야 하는데, 썩 그런 것 같지도 않다.
    
    아 시간은 참 모르겠다.

--- 1_PS/3_programmers/Level 3/Heap/디스크 컨트롤러.py
+++ 1_PS/3_programmers/Level 3/Heap/디스크 컨트롤러.py
@@ -1,11 +1,10 @@
 import heapq
 
-
 def solution(jobs):
-    jobs.sort()
+    jobs.sort(key=lambda x:[-x[0], -x[1]])
 
     work_count = len(jobs)
-    temp = jobs.pop(0)[::-1]
+    temp = jobs.pop()[::-1]
     wait_queue = [temp]
     acc = temp[1]
     answer = 0
@@ -16,11 +15,11 @@ def solution(jobs):
         answer += (acc - time[1])
 
         while jobs:
-            if jobs[0][0] <= acc:
-                heapq.heappush(wait_queue, jobs.pop(0)[::-1])
+            if jobs[-1][0] <= acc:
+                heapq.heappush(wait_queue, jobs.pop()[::-1])
             else:
                 if len(wait_queue) == 0:
-                    temp = jobs.pop(0)[::-1]
+                    temp = jobs.pop()[::-1]
                     heapq.heappush(wait_queue, temp)
                     acc = temp[1]
                 break
@@ -45,26 +44,26 @@ print(solution([[24, 10], [18, 39], [34, 20], [37, 5], [47, 22], [20, 47], [15, 
 # https://seoyoung2.github.io/algorithm/2020/06/04/Programmers-diskcontroller.html
 
 # 정확성  테스트
-# 테스트 1 〉	통과 (0.57ms, 10.2MB)
-# 테스트 2 〉	통과 (0.51ms, 10.2MB)
-# 테스트 3 〉	통과 (0.59ms, 10.4MB)
-# 테스트 4 〉	통과 (0.39ms, 10.2MB)
-# 테스트 5 〉	통과 (0.56ms, 10.2MB)
-# 테스트 6 〉	통과 (0.02ms, 10.3MB)
-# 테스트 7 〉	통과 (0.35ms, 10.2MB)
-# 테스트 8 〉	통과 (0.27ms, 10.2MB)
-# 테스트 9 〉	통과 (0.18ms, 10.3MB)
-# 테스트 10 〉	통과 (0.56ms, 10.3MB)
-# 테스트 11 〉	통과 (0.01ms, 10.2MB)
-# 테스트 12 〉	통과 (0.01ms, 10.3MB)
-# 테스트 13 〉	통과 (0.01ms, 10.3MB)
+# 테스트 1 〉	통과 (0.64ms, 10.3MB)
+# 테스트 2 〉	통과 (1.01ms, 10.3MB)
+# 테스트 3 〉	통과 (0.55ms, 10.3MB)
+# 테스트 4 〉	통과 (0.48ms, 10.3MB)
+# 테스트 5 〉	통과 (0.72ms, 10.3MB)
+# 테스트 6 〉	통과 (0.03ms, 10.2MB)
+# 테스트 7 〉	통과 (0.43ms, 10.2MB)
+# 테스트 8 〉	통과 (0.31ms, 10.3MB)
+# 테스트 9 〉	통과 (0.12ms, 10.3MB)
+# 테스트 10 〉	통과 (0.74ms, 10.2MB)
+# 테스트 11 〉	통과 (0.02ms, 10.2MB)
+# 테스트 12 〉	통과 (0.02ms, 10.3MB)
+# 테스트 13 〉	통과 (0.02ms, 10.2MB)
 # 테스트 14 〉	통과 (0.01ms, 10.2MB)
-# 테스트 15 〉	통과 (0.01ms, 10.2MB)
+# 테스트 15 〉	통과 (0.01ms, 10.3MB)
 # 테스트 16 〉	통과 (0.01ms, 10.2MB)
-# 테스트 17 〉	통과 (0.01ms, 10.4MB)
-# 테스트 18 〉	통과 (0.01ms, 10.3MB)
-# 테스트 19 〉	통과 (0.01ms, 10.1MB)
-# 테스트 20 〉	통과 (0.00ms, 10.2MB)
+# 테스트 17 〉	통과 (0.01ms, 10.3MB)
+# 테스트 18 〉	통과 (0.01ms, 10.2MB)
+# 테스트 19 〉	통과 (0.01ms, 10.3MB)
+# 테스트 20 〉	통과 (0.01ms, 10.2MB)
 #
 # 채점 결과
 # 정확성: 100.0

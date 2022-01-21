d68ac327a7123580711d56034b256aac36208059
mildsalmon_local_pc <mildsalmon@gmail.com>
2022-01-21 11:16:07 +0900

    :zap:[Edit] 디스크 컨트롤러 - 2차 시도 (테케 18개 맞음 - 실패)
    이 방법이 맞을텐데, 어떤 블로그를 찾아봐도 이런식으로 풀지 않았다.
    
    왜 비효율적으로 푸는건지 이해가 안가서 자료를 더 찾아봤다.
    
    어떤 블로그에서 테케를 적어둔걸 돌려봤는데, 오우.. 내가 acc 초기값을 잘못 설정해주고 있던 것이었다.
    
    그리고 또 다른 테케는 왜 틀렸는지 확인해봐야겠다.

--- 1_PS/3_programmers/Level 3/Heap/디스크 컨트롤러.py
+++ 1_PS/3_programmers/Level 3/Heap/디스크 컨트롤러.py
@@ -2,26 +2,26 @@ import heapq
 
 
 def solution(jobs):
-    jobs.sort(key=lambda x: [-x[0], -x[1]])
+    jobs.sort(key=lambda x: [x[0]])
 
-    wait_queue = [jobs.pop()[::-1]]
-    acc = 0
-    work_count = 0
+    work_count = len(jobs)
+    temp = jobs.pop(0)[::-1]
+    wait_queue = [temp]
+    acc = temp[1]
     answer = 0
 
     while wait_queue:
         time = heapq.heappop(wait_queue)
         acc += time[0]
-        work_count += 1
         answer += (acc - time[1])
 
         while jobs:
-            if jobs[-1][0] <= acc:
-                heapq.heappush(wait_queue, jobs.pop()[::-1])
+            if jobs[0][0] <= acc:
+                heapq.heappush(wait_queue, jobs.pop(0)[::-1])
             else:
                 if len(wait_queue) == 0:
                     temp = jobs.pop(0)[::-1]
-                    heapq.heappush(wait_queue, jobs.pop()[::-1])
+                    heapq.heappush(wait_queue, temp)
                     acc = temp[1]
                 break
 
@@ -29,4 +29,17 @@ def solution(jobs):
 
 
 
+print(solution([[0, 3], [1, 9], [2, 6]]))               # 9
+print(solution([[0, 3], [4, 3], [10, 3]]))              # 3
+print(solution([[0, 10], [2, 3], [9, 3]]))              # 9
+print(solution([[0, 10]]))                              # 10
+print(solution([[0, 10], [4, 10], [5, 11], [15, 2]]))   # 15
+print(solution([[24, 10], [18, 39], [34, 20], [37, 5], [47, 22], [20, 47], [15, 2], [15, 34], [35, 43], [26, 1]]))   # 74
 
+#### 아래 테케 틀림
+print(solution([[1, 10], [3, 3], [10, 3]]))             # 9
+print(solution([[24, 10], [18, 39], [34, 20], [37, 5], [47, 22], [20, 47], [15, 34], [15, 2], [35, 43], [26, 1]]))   # 74
+###
+
+# 테케 출처
+# https://seoyoung2.github.io/algorithm/2020/06/04/Programmers-diskcontroller.html

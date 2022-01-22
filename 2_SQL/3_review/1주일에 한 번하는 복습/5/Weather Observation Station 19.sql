9e93d852ef45fd49c53959a82420ead3e1ddd737
mildsalmon_local_pc <mildsalmon@gmail.com>
2022-01-22 15:34:33 +0900

    :tada:[Add] (복습) Weather Observation Station 19 - 1차 시도 (성공)

---
+++ 2_SQL/3_review/1주일에 한 번하는 복습/5/Weather Observation Station 19.sql
@@ -0,0 +1,13 @@
+/*
+Date    : 2022.01.22
+Update  : 2022.01.22
+Source  : Weather Observation Station 19.sql
+Purpose : euclidean distance / power / sqrt / round / max / min
+url     : https://www.hackerrank.com/challenges/weather-observation-station-19/problem?isFullScreen=true
+Author  : 김학진 (mildsalmon)
+Email   : mildsalmon@gamil.com
+*/
+
+SELECT ROUND(SQRT(POWER(MAX(lat_n)-min(lat_n), 2) + POWER(MAX(long_w)-MIN(long_w), 2)), 4)
+FROM station
+;

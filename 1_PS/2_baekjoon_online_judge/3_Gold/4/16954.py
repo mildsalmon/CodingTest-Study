cda755401d37b4a779a2b84cb412e61116ea348e
mildsalmon_local_pc <mildsalmon@gmail.com>
2022-01-27 10:39:38 +0900

    :zap:[Edit] 움직이는 미로 탈출 - 다른 사람 코드 추가
    시간이 광장히 짧아서 확인해봤는데, 와...
    단순히 체스판을 그림그리듯이 True / False로 가능한 경로만 남기고 지우는 방식으로 해결하셨다.
    이렇게도 할 수 있구나..

--- 1_PS/2_baekjoon_online_judge/3_Gold/4/16954.py
+++ 1_PS/2_baekjoon_online_judge/3_Gold/4/16954.py
@@ -87,3 +87,49 @@ if __name__ == "__main__":
     walls_list = map_move(walls, chess_map_size)
 
     print(bfs(init_pos, walls_list))
+
+"""
+다른 사람의 코드
+시간이 광장히 짧아서 확인해봤는데, 와...
+단순히 체스판을 그림그리듯이 True / False로 가능한 경로만 남기고 지우는 방식으로 해결하셨다.
+이렇게도 할 수 있구나..
+"""
+#
+# input = __import__('sys').stdin.readline
+# a = [input() for _ in range(8)]  # 체스판
+# d = [[False] * 8 for _ in range(8)]  # dp용 배열
+# d[7][0] = True
+# for i in range(8):  # 0초부터 7초까지 살펴봄
+#
+#     # -- 벽이 내려온 것을 다시 맞춰주는 작업-- --#
+#     # 0초일 때는 사실상 필요 없음
+#     for j in range(8 - i):
+#         for k in range(8):  # (j+1,k+1) 포인트
+#             if a[j][k] == '#':
+#                 d[i + j][k] = False
+#     # -- -- -- -- -- -- -- -- -- -- -- -- -- --#
+#     td = [[False] * 8 for _ in range(8)]  # true check
+#     # -- -- -- -- --케릭터가 갈 수 있는 곳을 True로 만들기-- -- -- --#
+#     for j in range(8):
+#         for k in range(8):
+#             for x in range(-1, 2):
+#                 for y in range(-1, 2):
+#                     # 순서대로 - [5]가 현재 위치
+#                     #  1 | 2 | 3
+#                     # --- --- ---
+#                     #  4 |[5]| 6
+#                     # --- --- ---
+#                     #  7 | 8 | 9
+#
+#                     if 0 <= j + x < 8 and 0 <= k + y < 8 and d[j + x][k + y]:
+#                         td[j][k] = True
+#     # -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --#
+#     for j in range(8 - i):
+#         for k in range(8):
+#             if a[j][k] == '#':
+#                 td[i + j][k] = False
+#     d = td
+#     # print(f'{i} 초 후')
+#     # [print(i) for i in td];print()
+#
+# print(1 if sum(map(sum, d)) else 0)

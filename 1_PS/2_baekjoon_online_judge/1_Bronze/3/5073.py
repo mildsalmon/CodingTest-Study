9b21cd6bbf625e37d8b76687b83f1ec2daca341c
mildsalmon_local_pc <mildsalmon@gmail.com>
2022-02-01 10:42:11 +0900

    :zap:[Edit] 삼각형과 세 변 - 2차 시도 (성공)

--- 1_PS/2_baekjoon_online_judge/1_Bronze/3/5073.py
+++ 1_PS/2_baekjoon_online_judge/1_Bronze/3/5073.py
@@ -12,16 +12,17 @@ if __name__ == "__main__":
 
     while True:
         num = list(map(int, input().split()))
-        num.sort()
 
         if num[0] == 0 and num[1] == 0 and num[2] == 0:
             break
 
-        if num[0] == num[1] and num[1] == num[2]:
-            print("Equilateral")
-        elif num[0] == num[1] or num[1] == num[2] or num[2] == num[0]:
-            print("Isosceles")
-        elif num[0] + num[1] <= num[2]:
+        num.sort()
+
+        if num[0] + num[1] <= num[2]:
             print("Invalid")
-        else:
+        elif num[0] != num[1] and num[1] != num[2] and num[2] != num[0]:
             print("Scalene")
+        elif num[0] != num[1] or num[1] != num[2] or num[2] != num[0]:
+            print("Isosceles")
+        else:
+            print("Equilateral")

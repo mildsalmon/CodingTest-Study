61effe13e0019719f1ea52deee570e09aed69dd2
mildsalmon_local_pc <mildsalmon@gmail.com>
2022-02-02 16:38:07 +0900

    :zap:[Edit] 01타일 - 2차 시도 (성공)

--- 1_PS/2_baekjoon_online_judge/2_Silver/3/1904.py
+++ 1_PS/2_baekjoon_online_judge/2_Silver/3/1904.py
@@ -2,7 +2,7 @@
 Date    : 2022.02.02
 Update  : 2022.02.02
 Source  : 1904.py
-Purpose : 정렬 후 양쪽 끝에서 다가오는 투포인터 문제
+Purpose : DP
 url     : https://www.acmicpc.net/problem/1904
 Author  : 김학진 (mildsalmon)
 Email   : mildsalmon@gamil.com
@@ -19,8 +19,7 @@ def tile(n):
         return num2
 
     for i in range(3, n+1):
-        num2 += num1
-        num1 = num2 - num1
+        num1, num2 = num2, (num1 + num2) % 15746
 
     return num2
 

9f810bbe0ea85f40101ee3830c0e574a3e228245
mildsalmon_local_pc <mildsalmon@gmail.com>
2022-02-09 11:38:00 +0900

    :tada:[Add] Draw The Triangle 1 - 1차 시도 (실패)
    이건 아니겠다 싶지만 해봤다.
    
    별이 역순으로 찍힌다.
    왜 정렬된거지?

---
+++ 2_SQL/2_HackerRank/1. Easy/Draw The Triangle 1.sql
@@ -0,0 +1,73 @@
+/*
+Date    : 2022.02.09
+Update  : 2022.02.09
+Source  : Draw The Triangle 1.sql
+Purpose : RPAD / CONNECT BY / LEVEL
+url     : https://www.hackerrank.com/challenges/draw-the-triangle-1/problem?isFullScreen=true
+Author  : 김학진 (mildsalmon)
+Email   : mildsalmon@gamil.com
+*/
+
+SELECT '********************'
+FROM dual
+UNION
+SELECT '*******************'
+FROM dual
+UNION
+SELECT '******************'
+FROM dual
+UNION
+SELECT '*****************'
+FROM dual
+UNION
+SELECT '****************'
+FROM dual
+UNION
+SELECT '***************'
+FROM dual
+UNION
+SELECT '**************'
+FROM dual
+UNION
+SELECT '*************'
+FROM dual
+UNION
+SELECT '************'
+FROM dual
+UNION
+SELECT '***********'
+FROM dual
+UNION
+SELECT '**********'
+FROM dual
+UNION
+SELECT '**********'
+FROM dual
+UNION
+SELECT '*********'
+FROM dual
+UNION
+SELECT '********'
+FROM dual
+UNION
+SELECT '*******'
+FROM dual
+UNION
+SELECT '******'
+FROM dual
+UNION
+SELECT '*****'
+FROM dual
+UNION
+SELECT '****'
+FROM dual
+UNION
+SELECT '***'
+FROM dual
+UNION
+SELECT '**'
+FROM dual
+UNION
+SELECT '*'
+FROM dual
+;

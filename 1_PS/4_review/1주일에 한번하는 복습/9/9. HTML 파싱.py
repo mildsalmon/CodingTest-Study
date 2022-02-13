90e2470929b6787faeb73f955298f87ea0ebdbbb
mildsalmon_local_pc <mildsalmon@gmail.com>
2022-02-13 15:22:22 +0900

    :zap:[Edit] (복습) HTML 파싱 (문자열 풀이)
    아직 멀었다.

--- 1_PS/4_review/1주일에 한번하는 복습/9/9. HTML 파싱.py
+++ 1_PS/4_review/1주일에 한번하는 복습/9/9. HTML 파싱.py
@@ -2,24 +2,41 @@
 Date    : 2022.02.13
 Update  : 2022.02.13
 Source  : 22859.py
-Purpose : 구현 / 문자열 / 파싱
+Purpose : 구현 / 문자열 / 파싱 / 정규식
 url     : https://www.acmicpc.net/problem/22859
 Author  : 김학진 (mildsalmon)
 Email   : mildsalmon@gamil.com
 """
 
-import re
+def parse(html):
+    print("title : " + html[len('<div title="'):html.index('">')])
+    html = html[html.index('">')+2:]
+    html = html.split('</p>')
+
+    for texts in html[:-1]:
+        sentence = ''
+        tag = False
+
+        for text in texts:
+            if text == '<':
+                tag = True
+            elif text == '>':
+                tag = False
+                continue
+
+            if not tag:
+                sentence += text
+        print(' '.join(sentence.split()))
 
 if __name__ == "__main__":
     html = input()
+    # html = '<main><div title="title_name_1"><p>paragraph 1</p><p>paragraph 2 <i>Italic Tag</i> <br > </p><p>paragraph 3 <b>Bold Tag</b> end.</p></div><div title="title_name_2"><p>paragraph 4</p><p>paragraph 5 <i>Italic Tag 2</i> <br > end.</p></div></main>'
 
     html = html[len('<main>'):-len('</main>')]
-    html = re.sub(r'<div title="([\w ]*)">', r'title : \1\n', html)
-    html = re.sub(r'</div>', '', html)
-    html = re.sub(r'<p>', '', html)
-    html = re.sub(r'</p>', '\n', html)
-    html = re.sub(r'</?[\w ]*>', '', html)
-    html = re.sub(r' *\n *', '\n', html)
-    html = re.sub(r' {2,}', ' ', html)
+    html = html.split('</div>')
 
-    print(html)
+    # print(*html, sep='\n')
+
+    for paragraph in html:
+        if paragraph:
+            parse(paragraph)

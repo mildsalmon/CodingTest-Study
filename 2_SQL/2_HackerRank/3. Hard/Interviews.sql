812bc6faae439636ef11b4d89040c089cfe37d35
mildsalmon_local_pc <mildsalmon@gmail.com>
2022-02-14 14:39:26 +0900

    :zap:[Edit] Interviews - 2차 시도 (성공)

--- 2_SQL/2_HackerRank/3. Hard/Interviews.sql
+++ 2_SQL/2_HackerRank/3. Hard/Interviews.sql
@@ -8,36 +8,30 @@ Author  : 김학진 (mildsalmon)
 Email   : mildsalmon@gamil.com
 */
 
-SELECT D.contest_id,
-       D.hacker_id,
-       D.name,
-       C.total_submissions,
-       C.total_accepted_submissions,
-       C.total_views,
-       C.total_unique_views
-FROM (
-    SELECT D.contest_id AS contest_id,
-           SUM(A.total_views) AS total_views,
-           SUM(A.total_unique_views) AS total_unique_views,
-           SUM(B.total_submissions) AS total_submissions,
-           SUM(B.total_accepted_submissions) AS total_accepted_submissions
-    FROM (
-        SELECT challenge_id,
-               SUM(total_views) AS total_views,
-               SUM(total_unique_views) AS total_unique_views
-        FROM view_stats
-        GROUP BY challenge_id
-    ) A JOIN (
-        SELECT challenge_id,
-               SUM(total_submissions) AS total_submissions,
-               SUM(total_accepted_submissions) AS total_accepted_submissions
-        FROM submission_stats
-        GROUP BY challenge_id
-    ) B ON (A.challenge_id = B.challenge_id)
-    JOIN challenges C ON (A.challenge_id = C.challenge_id)
-    JOIN colleges D ON (C.college_id = D.college_id)
-    WHERE A.total_views + A.total_unique_views + B.total_submissions + B.total_accepted_submissions != 0
-    GROUP BY D.contest_id
-) C JOIN contests D ON (C.contest_id = D.contest_id)
-ORDER BY D.contest_id ASC
+SELECT A.contest_id,
+       A.hacker_id,
+       A.name,
+       SUM(ss.total_submissions),
+       SUM(ss.total_accepted_submissions),
+       SUM(vs.total_views),
+       SUM(vs.total_unique_views)
+FROM contests A
+JOIN colleges B ON (A.contest_id = B.contest_id)
+JOIN challenges C ON (B.college_id = C.college_id)
+LEFT OUTER JOIN (
+    SELECT challenge_id,
+           SUM(total_views) AS total_views,
+           SUM(total_unique_views) AS total_unique_views
+    FROM view_stats
+    GROUP BY challenge_id
+) vs ON (C.challenge_id = vs.challenge_id)
+LEFT OUTER JOIN (
+    SELECT challenge_id,
+           SUM(total_submissions) AS total_submissions,
+           SUM(total_accepted_submissions) AS total_accepted_submissions
+    FROM submission_stats
+    GROUP BY challenge_id
+) ss ON (C.challenge_id = ss.challenge_id)
+GROUP BY A.contest_id, A.hacker_id, A.name
+ORDER BY A.contest_id ASC
 ;

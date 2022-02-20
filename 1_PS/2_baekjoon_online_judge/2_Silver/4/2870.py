de54f45cf8b87fc363db64bb4af9b5ea9d9340a1
mildsalmon_local_pc <mildsalmon@gmail.com>
2022-02-20 10:04:57 +0900

    :tada:[Add] 수학숙제 - 1차 시도 (실패)
    정규식에 너무 심취한 나머지 007은 int(007)을 하면 7이 나온다는 사실을 잊고 있었다.

---
+++ 1_PS/2_baekjoon_online_judge/2_Silver/4/2870.py
@@ -0,0 +1,27 @@
+"""
+Date    : 2022.02.20
+Update  : 2022.02.20
+Source  : 2870.py
+Purpose : re / 문자열 / 파싱
+url     : https://www.acmicpc.net/problem/2870
+Author  : 김학진 (mildsalmon)
+Email   : mildsalmon@gamil.com
+"""
+
+import re
+
+if __name__ == "__main__":
+    n = int(input())
+    arr = [input() for _ in range(n)]
+    re_arr = []
+
+    for s in arr:
+        temp = re.sub(r'\D', ' ', s)
+
+        for t in temp.split():
+            ttemp = re.sub(r'0*(\d)', r'\1', t)
+            re_arr.append(int(ttemp))
+
+    re_arr.sort()
+
+    print(*re_arr, sep='\n')

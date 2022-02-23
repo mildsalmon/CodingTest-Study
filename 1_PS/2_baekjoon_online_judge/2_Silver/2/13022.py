ce477db41947993f6a5fa0038cd88f895cebed98
mildsalmon_local_pc <mildsalmon@gmail.com>
2022-02-23 22:52:16 +0900

    :zap:[Edit] 늑대와 올바른 단어 - 정규식

--- 1_PS/2_baekjoon_online_judge/2_Silver/2/13022.py
+++ 1_PS/2_baekjoon_online_judge/2_Silver/2/13022.py
@@ -8,62 +8,32 @@ Author  : 김학진 (mildsalmon)
 Email   : mildsalmon@gamil.com
 """
 
+import re
 
-def check(seq, dic):
-    if ''.join(seq) == 'wolf' and dic['w'] == dic['o'] == dic['l'] == dic['f']:
-        seq.clear()
+def solution(text):
 
-        for key, value in dic.items():
-           dic[key] = 0
+    pattern = "(wolf"
+    for i in range(2, 13):
+        pattern += "|w{}{}{}o{}{}{}l{}{}{}f{}{}{}"\
+                .format('{', i, '}','{', i, '}','{', i, '}','{', i, '}',)
+    pattern += ")"
+
+    extra = re.sub(pattern, "", text)
+    if len(extra) > 0:
+        print(0)
     else:
-        return False
-    return True
+        print(1)
 
-
-def solution(word):
-    if len(word) < 4:
-        return 0
-
-    # wolf 순서 체크를 위한 seq
-    former = word[0]
-    seq = [former]
-
-    # wolf 개수 체크를 위한 dic
-    dic = dict(w=0, o=0, l=0, f=0)
-    dic[former] = 1
-    flag = True
-
-    for i in range(1, len(word)):
-        if word[i] != former:
-            if former == 'f':
-                flag = check(seq, dic)
-            seq.append(word[i])
-            former = word[i]
-
-        dic[word[i]] += 1
-
-        if i == len(word) - 1:
-            flag = check(seq, dic)
-
-        if not flag:
-            return 0
-
-    return 1
-
-
-if __name__ == "__main__":
-    print(solution(input()))
-
-    # print(solution('wolwolff'))
-    # print(solution('wolf'))
-    # print(solution('wwolfolf'))
-    # print(solution('wwwoolllfff'))
-    # print(solution('wwolfolf'))
-    # print(solution('wfol'))
-    # print(solution('wolfwwoollffwolf'))
-    # print(solution('wolf'))
-    # print(solution('wwoollff'))
-    # print(solution('wwwooolllfff'))
+solution('wolwolff')
+solution('wolf')
+solution('wwolfolf')
+solution('wwwoolllfff')
+solution('wwolfolf')
+solution('wfol')
+solution('wolfwwoollffwolf')
+solution('wolf')
+solution('wwoollff')
+solution('wwwooolllfff')
     # '''
     # 0
     # 1

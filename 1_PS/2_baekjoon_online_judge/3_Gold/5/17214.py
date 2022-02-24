10e444cdbff5ff78809f79746eb60d0d2548e6b5
mildsalmon_local_pc <mildsalmon@gmail.com>
2022-02-24 10:58:30 +0900

    :zap:[Edit] 다항 함수의 적분 - 3차 시도 (성공)

--- 1_PS/2_baekjoon_online_judge/3_Gold/5/17214.py
+++ 1_PS/2_baekjoon_online_judge/3_Gold/5/17214.py
@@ -18,46 +18,49 @@ def solution(fx):
     # elif fx[0] == '-':
     #     fx = '-1' + fx
 
-    fx = re.sub(r'(\d+)', r'\1x', fx)
-    num = fx.split('x')
+    if fx != '0':
+        fx = re.sub(r'(\d+)', r'\1x', fx)
+        num = fx.split('x')
 
-    # print(fx)
+        # print(fx)
 
-    if len(num) > 2:
-        num[0] = int(num[0]) // 2
-    else:
-        num[0] = int(num[0])
-
-    if num[0] != 1:
-        if num[0] == -1:
-            answer += '-'
+        if len(num) > 2:
+            num[0] = int(num[0]) // 2
         else:
-            answer += str(num[0])
+            num[0] = int(num[0])
 
-    if len(num) == 2:
-        answer += 'x'
-    else:
-        answer += 'xx'
-
-    if len(num) == 4:
-        num[2] = int(num[2])
-
-        if num[2] != 1:
-            if num[2] == -1:
+        if num[0] != 1:
+            if num[0] == -1:
                 answer += '-'
-            elif num[2] > 0:
-                answer += '+' + str(num[2])
-            elif num[2] < 0:
-                answer += str(num[2])
+            else:
+                answer += str(num[0])
+
+        if len(num) == 2:
+            answer += 'x'
         else:
-            answer += '+'
+            answer += 'xx'
 
-        answer += 'x'
+        if len(num) == 4:
+            num[2] = int(num[2])
 
-    answer += '+W'
-    # print(answer)
-    # print(num)
+            if num[2] != 1:
+                if num[2] == -1:
+                    answer += '-'
+                elif num[2] > 0:
+                    answer += '+' + str(num[2])
+                elif num[2] < 0:
+                    answer += str(num[2])
+            else:
+                answer += '+'
+
+            answer += 'x'
+
+        answer += '+W'
+    else:
+        answer = 'W'
+
     print(answer)
+
 
 if __name__ == "__main__":
     # fx = input()

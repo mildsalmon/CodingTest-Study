6ba3066953c37a412d7a8df6d12b304379f76961
mildsalmon_local_pc <mildsalmon@gmail.com>
2022-03-02 06:16:59 +0900

    :zap:[Edit] 도시 건설 - 2차 시도 (성공 : 속도 개선)

--- 1_PS/2_baekjoon_online_judge/3_Gold/4/21924.py
+++ 1_PS/2_baekjoon_online_judge/3_Gold/4/21924.py
@@ -2,7 +2,7 @@
 Date    : 2022.03.02
 Update  : 2022.03.02
 Source  : 21924.py
-Purpose :
+Purpose : 크루스칼 / union-find
 url     : https://www.acmicpc.net/problem/21924
 Author  : 김학진 (mildsalmon)
 Email   : mildsalmon@gamil.com
@@ -28,6 +28,23 @@ def union_parent(parent: list, a: int, b: int):
         parent[b] = a
 
 
+def solution(ways: list, total_cost: int) -> int:
+    global n
+
+    parent = [i for i in range(n+1)]
+
+    for cost, a, b in ways:
+        if find_parent(parent, a) != find_parent(parent, b):
+            union_parent(parent, a, b)
+            total_cost -= cost
+
+    for i in range(1, n+1):
+        if find_parent(parent, parent[i]) != 1:
+            return -1
+
+    return total_cost
+
+
 if __name__ == "__main__":
     n, m = list(map(int, input().split()))
     ways = []
@@ -40,18 +57,5 @@ if __name__ == "__main__":
 
     ways.sort()
 
-    parent = [i for i in range(n+1)]
+    print(solution(ways, total_cost))
 
-    for cost, a, b in ways:
-        if find_parent(parent, a) != find_parent(parent, b):
-            union_parent(parent, a, b)
-            total_cost -= cost
-
-    for i in range(1, n+1):
-        if parent[i] != 1:
-            find_parent(parent, i)
-
-    if sum(parent) == n:
-        print(total_cost)
-    else:
-        print(-1)

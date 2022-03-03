c5050e18aa5f4ca37a02f063a500eb40072b243a
mildsalmon_local_pc <mildsalmon@gmail.com>
2022-03-03 10:42:05 +0900

    :zap:[Edit] 곡예 비행 - 2차 시도 (성공)

--- 1_PS/2_baekjoon_online_judge/3_Gold/4/21923.py
+++ 1_PS/2_baekjoon_online_judge/3_Gold/4/21923.py
@@ -11,45 +11,39 @@ Email   : mildsalmon@gamil.com
 if __name__ == "__main__":
     n, m = list(map(int, input().split()))
     array = [list(map(int, input().split())) for _ in range(n)]
-    dp = [[-1e9]*m for _ in range(n)]
+    dp_up = [[-1e9]*m for _ in range(n)]
+    dp_down = [[-1e9]*m for _ in range(n)]
 
-    answer = -1e9
+    dp_up[n-1][0] = array[n-1][0]
 
-    # 상승
     for i in range(n-1, -1, -1):
         for j in range(m):
             if i == n-1 and j == 0:
-                temp = array[i][j]
-            elif i == n-1:
-                temp = array[i][j] + dp[i][j-1]
-            elif j == 0:
-                temp = array[i][j] + dp[i+1][j]
-            else:
-                temp = max(dp[i+1][j], dp[i][j-1]) + array[i][j]
+                continue
 
-            if answer < temp:
-                answer = temp
-                dp[i][j] = temp
-            else:
-                j -= 1
-                break
+            if i < n-1:
+                dp_up[i][j] = max(dp_up[i][j], dp_up[i+1][j])
+            if j > 0:
+                dp_up[i][j] = max(dp_up[i][j], dp_up[i][j-1])
+            dp_up[i][j] += array[i][j]
 
-    # print(*dp, sep='\n')
-    # print(i, j)
+    dp_down[n-1][m-1] = array[n-1][m-1]
 
-    for ii in range(i, n):
-        for jj in range(j, m):
-            if ii == i and jj == j:
-                temp = array[ii][jj] + dp[ii][jj]
-            elif ii == i:
-                temp = array[ii][jj] + dp[ii][jj-1]
-            elif jj == j-1:
-                temp = array[ii][jj] + dp[ii-1][jj]
-            else:
-                temp = max(dp[ii-1][jj], dp[ii][jj-1]) + array[ii][jj]
+    for i in range(n-1, -1, -1):
+        for j in range(m-1, -1, -1):
+            if i == n-1 and j == m-1:
+                continue
 
-            dp[ii][jj] = temp
+            if i < n-1:
+                dp_down[i][j] = max(dp_down[i][j], dp_down[i+1][j])
+            if j < m-1:
+                dp_down[i][j] = max(dp_down[i][j], dp_down[i][j+1])
+            dp_down[i][j] += array[i][j]
 
-    # print(*dp, sep='\n')
+    answer = -1e9
 
-    print(dp[n-1][m-1])
+    for i in range(n):
+        for j in range(m):
+            answer = max(answer, dp_up[i][j] + dp_down[i][j])
+
+    print(answer)

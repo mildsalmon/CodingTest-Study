8d0b94f9663a2ee155ca8aab41d7ba44c56e3392
mildsalmon_local_pc <mildsalmon@gmail.com>
2022-03-05 13:48:45 +0900

    :zap:[Edit] (복습) 곡예 비행 - 1차 시도 (성공)

--- 1_PS/2_baekjoon_online_judge/3_Gold/4/21923.py
+++ 1_PS/2_baekjoon_online_judge/3_Gold/4/21923.py
@@ -1,6 +1,6 @@
 """
 Date    : 2022.03.03
-Update  : 2022.03.03
+Update  : 2022.03.05
 Source  : 21923.py
 Purpose : dp
 url     : https://www.acmicpc.net/problem/21923
@@ -10,40 +10,44 @@ Email   : mildsalmon@gamil.com
 
 if __name__ == "__main__":
     n, m = list(map(int, input().split()))
-    array = [list(map(int, input().split())) for _ in range(n)]
-    dp_up = [[-1e9]*m for _ in range(n)]
-    dp_down = [[-1e9]*m for _ in range(n)]
-
-    dp_up[n-1][0] = array[n-1][0]
-
-    for i in range(n-1, -1, -1):
-        for j in range(m):
-            if i == n-1 and j == 0:
-                continue
-
-            if i < n-1:
-                dp_up[i][j] = max(dp_up[i][j], dp_up[i+1][j])
-            if j > 0:
-                dp_up[i][j] = max(dp_up[i][j], dp_up[i][j-1])
-            dp_up[i][j] += array[i][j]
-
-    dp_down[n-1][m-1] = array[n-1][m-1]
-
-    for i in range(n-1, -1, -1):
-        for j in range(m-1, -1, -1):
-            if i == n-1 and j == m-1:
-                continue
-
-            if i < n-1:
-                dp_down[i][j] = max(dp_down[i][j], dp_down[i+1][j])
-            if j < m-1:
-                dp_down[i][j] = max(dp_down[i][j], dp_down[i][j+1])
-            dp_down[i][j] += array[i][j]
-
+    graph = [list(map(int, input().split())) for _ in range(n)]
     answer = -1e9
 
-    for i in range(n):
-        for j in range(m):
-            answer = max(answer, dp_up[i][j] + dp_down[i][j])
+    dp_up = [[-1e9] * m for _ in range(n)]
+    dp_down = [[-1e9] * m for _ in range(n)]
+
+    dp_up[n-1][0] = graph[n-1][0]
+
+    for r in range(n-1, -1, -1):
+        for c in range(m):
+            if r == n-1 and c == 0:
+                continue
+
+            if r == n-1:
+                dp_up[r][c] = max(dp_up[r][c-1] + graph[r][c], dp_up[r][c])
+            if c == 0:
+                dp_up[r][c] = max(dp_up[r+1][c] + graph[r][c], dp_up[r][c])
+            if r != n-1 and c != 0:
+                dp_up[r][c] = max(max(dp_up[r][c-1], dp_up[r+1][c]) + graph[r][c], dp_up[r][c])
+
+    # print(*dp_up, sep='\n')
+
+    dp_down[n-1][m-1] = graph[n-1][m-1]
+
+    for r in range(n-1, -1, -1):
+        for c in range(m-1, -1, -1):
+            if r == n-1 and c == m-1:
+                continue
+
+            if r == n-1:
+                dp_down[r][c] = max(dp_down[r][c+1] + graph[r][c], dp_down[r][c])
+            if c == m-1:
+                dp_down[r][c] = max(dp_down[r+1][c] + graph[r][c], dp_down[r][c])
+            if r != n-1 and c != m-1:
+                dp_down[r][c] = max(max(dp_down[r+1][c], dp_down[r][c+1]) + graph[r][c], dp_down[r][c])
+
+    for r in range(n):
+        for c in range(m):
+            answer = max(answer, dp_up[r][c] + dp_down[r][c])
 
     print(answer)

fc7ad4d4350f8b79cf1cedb6b62dde9c924d3122
mildsalmon_local_pc <mildsalmon@gmail.com>
2022-03-10 10:40:47 +0900

    :zap:[Edit] (틀린거 쭉 복습) 15 Days of Learning SQL - 1차 시도 (성공)

--- 2_SQL/2_HackerRank/3. Hard/15 Days of Learning SQL.sql
+++ 2_SQL/2_HackerRank/3. Hard/15 Days of Learning SQL.sql
@@ -1,6 +1,6 @@
 /*
 Date    : 2022.02.18
-Update  : 2022.02.18
+Update  : 2022.03.10
 Source  : 15 Days of Learning SQL.sql
 Purpose : JOIN
 url     : https://www.hackerrank.com/challenges/15-days-of-learning-sql/problem?isFullScreen=true
@@ -8,36 +8,33 @@ Author  : 김학진 (mildsalmon)
 Email   : mildsalmon@gamil.com
 */
 
-SELECT B.s_date
-     , A.cnt
-     , hackers.hacker_id
-     , hackers.name
+SELECT X.submission_date
+     , X.cnt
+     , Y.hacker_id
+     , Z.name
 FROM (
-    SELECT s.submission_date AS s_date
-         , COUNT(DISTINCT(s.hacker_id)) AS cnt
-    FROM submissions s
-    WHERE (s.submission_date - TO_DATE('2016-03-01')) = (
-        SELECT COUNT(DISTINCT(ss.submission_date))
-        FROM submissions ss
-        WHERE s.submission_date > ss.submission_date
-            AND s.hacker_id = ss.hacker_id
+    SELECT A.submission_date
+         , COUNT(DISTINCT A.hacker_id) AS cnt
+    FROM submissions A
+    WHERE (A.submission_date - TO_DATE('2016-03-01')) = (
+        SELECT COUNT(DISTINCT B.submission_date)
+        FROM submissions B
+        WHERE A.submission_date > B.submission_date
+            AND A.hacker_id = B.hacker_id
     )
-    GROUP BY s.submission_date
-) A JOIN (
-    SELECT s_date
-         , id
-         , ROW_NUMBER() OVER(PARTITION BY s_date
-                            ORDER BY cnt DESC,
-                                     id ASC) AS num
+    GROUP BY A.submission_date
+) X LEFT JOIN (
+    SELECT D.submission_date
+         , D.hacker_id
+         , ROW_NUMBER() OVER(PARTITION BY D.submission_date ORDER BY D.cnt DESC, D.hacker_id ASC) AS rank
     FROM (
-        SELECT s.submission_date AS s_date
-             , s.hacker_id AS id
-             , COUNT(s.hacker_id) AS cnt
-        FROM submissions s
-        GROUP BY s.submission_date, s.hacker_id
-    )
-) B ON (B.num = 1
-       AND A.s_date = B.s_date)
-JOIN hackers ON (B.id = hackers.hacker_id)
-ORDER BY B.s_date ASC
+        SELECT C.submission_date
+             , C.hacker_id
+             , COUNT(1) AS cnt
+        FROM submissions C
+        GROUP BY C.submission_date, C.hacker_id
+    ) D
+) Y ON (Y.rank = 1 AND X.submission_date = Y.submission_date)
+JOIN hackers Z ON (Y.hacker_id = Z.hacker_id)
+ORDER BY 1
 ;

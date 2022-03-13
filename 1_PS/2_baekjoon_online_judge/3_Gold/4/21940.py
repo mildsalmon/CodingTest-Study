6d77f000a7375c089d98645740843816c5914b20
mildsalmon_local_pc <mildsalmon@gmail.com>
2022-03-13 12:30:10 +0900

    :zap:[Edit] (복습) 가운데에서 만나기 - 1차 시도 (성공)

--- 1_PS/2_baekjoon_online_judge/3_Gold/4/21940.py
+++ 1_PS/2_baekjoon_online_judge/3_Gold/4/21940.py
@@ -1,6 +1,6 @@
 """
 Date    : 2022.03.09
-Update  : 2022.03.09
+Update  : 2022.03.13
 Source  : 21940.py
 Purpose : 플로이드-와샬
 url     : https://www.acmicpc.net/problem/21940
@@ -15,34 +15,39 @@ if __name__ == "__main__":
     n, m = list(map(int, input().split()))
     graph = [[1e9] * (n+1) for _ in range(n+1)]
 
-    for i in range(n+1):
+    for i in range(1, n+1):
         graph[i][i] = 0
 
     for _ in range(m):
-        a, b, cost = list(map(int, input().split()))
+        a, b, time = list(map(int, input().split()))
 
-        graph[a][b] = cost
+        graph[a][b] = time
 
     for k in range(1, n+1):
         for i in range(1, n+1):
             for j in range(1, n+1):
                 graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
 
+    # print(*graph, sep='\n')
+
     k = int(input())
-    friends = list(map(int, input().split()))
-    citys = []
+    c = list(map(int, input().split()))
+
+    dest_citys = []
+    min_distance = 1e9
 
     for i in range(1, n+1):
-        max_cost = 0
-        for j in friends:
-            max_cost = max(max_cost, graph[j][i] + graph[i][j])
-        citys.append([max_cost, i])
+        max_distance = 0
 
-    citys.sort(key=lambda x: [x[0], x[1]])
+        for j in c:
+            max_distance = max(max_distance, graph[i][j] + graph[j][i])
 
-    # print(*graph, sep='\n')
-    # print(citys)
+        if min_distance < max_distance:
+            continue
+        elif min_distance > max_distance:
+            min_distance = max_distance
+            dest_citys = []
+        dest_citys.append(i)
+    dest_citys.sort()
 
-    for city in citys:
-        if city[0] == citys[0][0]:
-            print(city[1], end=' ')
+    print(*dest_citys, sep=' ')

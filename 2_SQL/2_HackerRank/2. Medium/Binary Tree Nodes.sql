1992640d6a07567d40fdb49775823ba5309f44e4
mildsalmon_local_pc <mildsalmon@gmail.com>
2022-03-14 09:59:33 +0900

    :zap:[Edit] (틀린거 쭉 복습) Binary Tree Nodes - 1차 시도 (성공)

--- 2_SQL/2_HackerRank/2. Medium/Binary Tree Nodes.sql
+++ 2_SQL/2_HackerRank/2. Medium/Binary Tree Nodes.sql
@@ -1,6 +1,6 @@
 /*
 Date    : 2022.01.03
-Update  : 2022.03.07
+Update  : 2022.03.14
 Source  : Binary Tree Nodes.sql
 Purpose : 계층형 질의 / start with / connect by / connect_by_isleaf / level
 url     : https://www.hackerrank.com/challenges/binary-search-tree-1/problem?isFullScreen=true
@@ -8,12 +8,12 @@ Author  : 김학진 (mildsalmon)
 Email   : mildsalmon@gamil.com
 */
 
-SELECT N
-    , CASE
+SELECT N,
+    CASE
         WHEN LEVEL = 1 THEN 'Root'
         WHEN CONNECT_BY_ISLEAF = 1 THEN 'Leaf'
         ELSE 'Inner'
-      END
+    END
 FROM bst
 START WITH P IS NULL
 CONNECT BY PRIOR N = P

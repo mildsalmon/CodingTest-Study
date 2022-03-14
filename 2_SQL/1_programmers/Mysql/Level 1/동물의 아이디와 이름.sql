106253a0c7aa5993f059fdf0d59638522a6833aa
mildsalmon_local_pc <mildsalmon@gmail.com>
2022-03-14 10:16:46 +0900

    :tada:[ADD] 동물의 아이디와 이름 - 1차 시도 (성공)

---
+++ 2_SQL/1_programmers/Mysql/Level 1/동물의 아이디와 이름.sql
@@ -0,0 +1,15 @@
+/*
+Date    : 2021.03.14
+Update  : 2021.03.14
+Source  : 동물의 아이디와 이름.sql
+Purpose : 조건에 맞춰서 정렬
+url     : https://programmers.co.kr/learn/courses/30/lessons/59403?language=mysql
+Author  : 김학진 (mildsalmon)
+Email   : mildsalmon@gamil.com
+*/
+
+SELECT ANIMAL_ID
+     , NAME
+FROM ANIMAL_INS
+ORDER BY 1 ASC
+;

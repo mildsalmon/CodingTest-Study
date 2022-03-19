9fc6d91f5a4a6797c3a81bad842c5621c5f810e0
mildsalmon_local_pc <mildsalmon@gmail.com>
2022-03-19 09:47:30 +0900

    :zap:[Edit] (복습) 물통 - 2차 시도 (실패 - 틀림)
    C 물통이 0일 가능성도 있다.
    모든 물통이 0이거나 A와 C 물통만 0이거나

--- 1_PS/2_baekjoon_online_judge/2_Silver/1/2251.py
+++ 1_PS/2_baekjoon_online_judge/2_Silver/1/2251.py
@@ -10,8 +10,14 @@ Email   : mildsalmon@gamil.com
 from collections import deque
 
 
-def water_move(water_amount: set):
+def water_move(water_amount: set, visited: list):
     global a, b, c
+
+    def check(x, y) -> bool:
+        if visited[x][y]:
+            return False
+        visited[x][y] = True
+        return True
 
     q = deque()
     q.append((0, 0))
@@ -20,42 +26,47 @@ def water_move(water_amount: set):
         x, y = q.popleft()
         z = c - (x + y)
 
-        if z in water_amount:
-            continue
-
         if x == 0 and z > 0:
             water_amount.add(z)
 
         # x -> y
-        temp_y = min(x, b)
-        q.append((x - temp_y, temp_y))
+        temp_y = min(x, b-y)
+        if check(x-temp_y, y+temp_y):
+            q.append((x-temp_y, y+temp_y))
 
         # x -> z
-        temp_z = min(x, c)
-        q.append((x - temp_z, y))
+        temp_z = min(x, c-z)
+        if check(x-temp_z, y):
+            q.append((x-temp_z, y))
 
         # y -> x
-        temp_x = min(y, a)
-        q.append((temp_x, y - temp_x))
+        temp_x = min(y, a-x)
+        if check(temp_x+x, y-temp_x):
+            q.append((temp_x+x, y-temp_x))
 
         # y -> z
-        temp_z = min(y, c)
-        q.append((x, y - temp_z))
+        temp_z = min(y, c-z)
+        if check(x, y-temp_z):
+            q.append((x, y-temp_z))
 
         # z -> x
-        temp_x = min(z, a)
-        q.append((temp_x, y))
+        temp_x = min(z, a-x)
+        if check(temp_x+x, y):
+            q.append((temp_x+x, y))
 
         # z -> y
-        temp_y = min(z, b)
-        q.append((x, temp_y))
+        temp_y = min(z, b-y)
+        if check(x, temp_y+y):
+            q.append((x, temp_y+y))
 
 
 if __name__ == "__main__":
     a, b, c = list(map(int, input().split()))
 
+    visited = [[False for _ in range(b+1)] for _ in range(a+1)]
     water_amount = set()
-    water_move(water_amount)
+
+    water_move(water_amount, visited)
 
     answer = sorted(water_amount)
-    print(*answer)
+    print(*answer, sep=' ')

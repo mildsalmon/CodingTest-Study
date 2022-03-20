619461bda4a4e571bdbcb6ef022ed23e9edd5369
mildsalmon_local_pc <mildsalmon@gmail.com>
2022-03-21 08:56:16 +0900

    :zap:[Edit] (복습) 완주하지 못한 선수 - 1차 시도 (성공)

--- 1_PS/3_programmers/Level 1/해시/완주하지 못한 선수.py
+++ 1_PS/3_programmers/Level 1/해시/완주하지 못한 선수.py
@@ -1,44 +1,23 @@
 """
 Date    : 2021.12.14
-Update  : 2021.12.14
+Update  : 2021.03.21
 Source  : 완주하지 못한 선수.py
 Purpose : 해시를 사용하여 해결
 Author  : 김학진 (mildsalmon)
 Email   : mildsalmon@gamil.com
 """
 
+
 def solution(participant, completion):
-    """
-    여러가지 방법을 생각하다가 시간복잡도 문제때문에 dict()을 사용하여 단순하게 풀었다.
-    """
-    check = {}
-    for part in participant:
-        if part in check:
-            check[part] += 1
-        else:
-            check[part] = 1
+    hash_sum = 0
+    hash_dict = dict()
 
-    for com in completion:
-        if com in check:
-            check[com] -= 1
+    for people in participant:
+        if people not in hash_dict:
+            hash_dict[hash(people)] = people
+        hash_sum += hash(people)
 
-    for c in check:
-        if check[c] == 1:
-            return c
+    for people in completion:
+        hash_sum -= hash(people)
 
-
-
-# 다른 사람의 풀이
-
-# def solution(participant, completion):
-#     answer = ''
-#     temp = 0
-#     dic = {}
-#     for part in participant:
-#         dic[hash(part)] = part
-#         temp += int(hash(part))
-#     for com in completion:
-#         temp -= hash(com)
-#     answer = dic[temp]
-#
-#     return answer
+    return hash_dict[hash_sum]

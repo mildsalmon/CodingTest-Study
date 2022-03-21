46ffe26d0943e5360d895137f4b37fbfade76bc6
mildsalmon_local_pc <mildsalmon@gmail.com>
2022-03-21 10:03:26 +0900

    :tada:[Add] 중복 제거하기 - 1차 시도 (성공)

---
+++ 2_SQL/1_programmers/Mysql/Level 2/중복 제거하기.sql
@@ -0,0 +1,13 @@
+/*
+Date    : 2021.03.21
+Update  : 2021.03.21
+Source  : 중복 제거하기.sql
+Purpose : COUNT / DISTINCT
+url     : https://programmers.co.kr/learn/courses/30/lessons/59408?language=mysql
+Author  : 김학진 (mildsalmon)
+Email   : mildsalmon@gamil.com
+*/
+
+SELECT MIN(DATETIME)
+FROM ANIMAL_INS
+;

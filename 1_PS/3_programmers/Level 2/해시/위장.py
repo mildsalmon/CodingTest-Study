1eb8ceefcc4836061a14b2562ae53681c15fd27f
mildsalmon_local_pc <mildsalmon@gmail.com>
2022-03-21 09:40:28 +0900

    :zap:[Edit] (복습) 위장 - 2차 시도 (성공)
    Counter 클래스를 사용하면 시간이 조금 오래 걸리는 것을 알 수 있다.

--- 1_PS/3_programmers/Level 2/해시/위장.py
+++ 1_PS/3_programmers/Level 2/해시/위장.py
@@ -9,54 +9,44 @@ Email   : mildsalmon@gamil.com
 """
 
 from functools import reduce
+from collections import Counter
 
 
 def solution(clothes):
-    answer = 0
-
-    kind_by_name = dict()
-
-    for name, kind in clothes:
-        if kind in kind_by_name:
-            kind_by_name[kind].append(name)
-        else:
-            kind_by_name[kind] = [name]
-
-    # print(kind_by_name)
-
-    answer = reduce(lambda y, x: y * (len(x) + 1), kind_by_name.values(), 1) - 1
+    kind_by_cnt = Counter(list(map(lambda x: x[1], clothes)))
+    answer = reduce(lambda y, x: y * (x + 1), kind_by_cnt.values(), 1) - 1
 
     return answer
 
 # 정확성  테스트
-# 테스트 1 〉	통과 (0.01ms, 10.2MB)
-# 테스트 2 〉	통과 (0.01ms, 10.1MB)
-# 테스트 3 〉	통과 (0.01ms, 10.2MB)
-# 테스트 4 〉	통과 (0.01ms, 10.1MB)
-# 테스트 5 〉	통과 (0.01ms, 10.3MB)
-# 테스트 6 〉	통과 (0.01ms, 10.3MB)
-# 테스트 7 〉	통과 (0.01ms, 10.3MB)
-# 테스트 8 〉	통과 (0.01ms, 10.3MB)
-# 테스트 9 〉	통과 (0.01ms, 10.4MB)
-# 테스트 10 〉	통과 (0.01ms, 10.2MB)
-# 테스트 11 〉	통과 (0.01ms, 10.3MB)
-# 테스트 12 〉	통과 (0.02ms, 10.1MB)
-# 테스트 13 〉	통과 (0.01ms, 10.1MB)
-# 테스트 14 〉	통과 (0.02ms, 10.2MB)
-# 테스트 15 〉	통과 (0.01ms, 10.1MB)
-# 테스트 16 〉	통과 (0.00ms, 10.2MB)
-# 테스트 17 〉	통과 (0.01ms, 10.2MB)
-# 테스트 18 〉	통과 (0.01ms, 10.1MB)
-# 테스트 19 〉	통과 (0.01ms, 10.3MB)
-# 테스트 20 〉	통과 (0.01ms, 10.2MB)
-# 테스트 21 〉	통과 (0.01ms, 10.2MB)
-# 테스트 22 〉	통과 (0.01ms, 10.4MB)
-# 테스트 23 〉	통과 (0.01ms, 10.2MB)
-# 테스트 24 〉	통과 (0.01ms, 10.3MB)
-# 테스트 25 〉	통과 (0.01ms, 10.2MB)
-# 테스트 26 〉	통과 (0.01ms, 10.2MB)
-# 테스트 27 〉	통과 (0.01ms, 10.2MB)
-# 테스트 28 〉	통과 (0.01ms, 10.4MB)
+# 테스트 1 〉	통과 (0.03ms, 10.2MB)
+# 테스트 2 〉	통과 (0.03ms, 10.3MB)
+# 테스트 3 〉	통과 (0.03ms, 10.1MB)
+# 테스트 4 〉	통과 (0.03ms, 10.1MB)
+# 테스트 5 〉	통과 (0.04ms, 10.1MB)
+# 테스트 6 〉	통과 (0.02ms, 10.3MB)
+# 테스트 7 〉	통과 (0.04ms, 10.1MB)
+# 테스트 8 〉	통과 (0.03ms, 10.1MB)
+# 테스트 9 〉	통과 (0.02ms, 10.1MB)
+# 테스트 10 〉	통과 (0.03ms, 10.2MB)
+# 테스트 11 〉	통과 (0.03ms, 10.1MB)
+# 테스트 12 〉	통과 (0.03ms, 10.2MB)
+# 테스트 13 〉	통과 (0.03ms, 10.2MB)
+# 테스트 14 〉	통과 (0.02ms, 10.1MB)
+# 테스트 15 〉	통과 (0.03ms, 10.1MB)
+# 테스트 16 〉	통과 (0.02ms, 9.87MB)
+# 테스트 17 〉	통과 (0.03ms, 10.2MB)
+# 테스트 18 〉	통과 (0.03ms, 10.1MB)
+# 테스트 19 〉	통과 (0.03ms, 10.1MB)
+# 테스트 20 〉	통과 (0.03ms, 10.1MB)
+# 테스트 21 〉	통과 (0.02ms, 10.2MB)
+# 테스트 22 〉	통과 (0.03ms, 10.3MB)
+# 테스트 23 〉	통과 (0.02ms, 10.1MB)
+# 테스트 24 〉	통과 (0.02ms, 10MB)
+# 테스트 25 〉	통과 (0.03ms, 10.1MB)
+# 테스트 26 〉	통과 (0.03ms, 10.1MB)
+# 테스트 27 〉	통과 (0.03ms, 10.1MB)
+# 테스트 28 〉	통과 (0.03ms, 10.2MB)
 #
 # 채점 결과
 # 정확성: 100.0

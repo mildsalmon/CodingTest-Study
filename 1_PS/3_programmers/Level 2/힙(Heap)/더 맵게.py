f5ef0e01253712e0b8f4b542a9b76dae2e380005
mildsalmon_local_pc <mildsalmon@gmail.com>
2022-03-22 10:08:43 +0900

    :zap:[Edit] (복습) 더 맵게 - 1차 시도 (성공)

--- 1_PS/3_programmers/Level 2/힙(Heap)/더 맵게.py
+++ 1_PS/3_programmers/Level 2/힙(Heap)/더 맵게.py
@@ -1,6 +1,6 @@
 """
 Date    : 2022.01.04
-Update  : 2022.01.04
+Update  : 2022.03.22
 Source  : 더 맵게.py
 Purpose : heap / heapify
 url     : https://programmers.co.kr/learn/courses/30/lessons/42626
@@ -10,50 +10,50 @@ Email   : mildsalmon@gamil.com
 
 import heapq
 
-def solution(scoville: list, K: int) -> int:
+
+def solution(scoville, K):
+    answer = 0
+
     heapq.heapify(scoville)
-    count = 0
 
     while scoville[0] < K:
-        if len(scoville) < 2:
+        if len(scoville) <= 1:
             return -1
 
-        food_A = heapq.heappop(scoville)
-        food_B = heapq.heappop(scoville)
+        non_spicy_1 = heapq.heappop(scoville)
+        non_spicy_2 = heapq.heappop(scoville)
+        new_scoville = non_spicy_1 + (non_spicy_2 * 2)
 
-        mix_food = food_A + 2 * food_B
+        heapq.heappush(scoville, new_scoville)
+        answer += 1
 
-        heapq.heappush(scoville, mix_food)
-        count += 1
-
-    return count
+    return answer
 
 # 정확성  테스트
 #
-# 테스트 1 〉	통과 (0.01ms, 10.2MB)
-# 테스트 2 〉	통과 (0.01ms, 10.2MB)
-# 테스트 3 〉	통과 (0.01ms, 10.2MB)
-# 테스트 4 〉	통과 (0.01ms, 10.3MB)
-# 테스트 5 〉	통과 (0.01ms, 10.2MB)
-# 테스트 6 〉	통과 (0.72ms, 10.2MB)
-# 테스트 7 〉	통과 (0.68ms, 10.2MB)
-# 테스트 8 〉	통과 (0.05ms, 10.2MB)
-# 테스트 9 〉	통과 (0.09ms, 10.2MB)
-# 테스트 10 〉	통과 (0.29ms, 10.2MB)
-# 테스트 11 〉	통과 (0.19ms, 10.3MB)
-# 테스트 12 〉	통과 (1.36ms, 10.2MB)
-# 테스트 13 〉	통과 (0.36ms, 10.4MB)
-# 테스트 14 〉	통과 (0.01ms, 10.2MB)
-# 테스트 15 〉	통과 (0.70ms, 10.3MB)
-# 테스트 16 〉	통과 (0.00ms, 10.2MB)
+# 테스트 1 〉	통과 (0.01ms, 10.1MB)
+# 테스트 2 〉	통과 (0.00ms, 10.1MB)
+# 테스트 3 〉	통과 (0.02ms, 10.2MB)
+# 테스트 4 〉	통과 (0.01ms, 10.1MB)
+# 테스트 5 〉	통과 (0.01ms, 10.1MB)
+# 테스트 6 〉	통과 (0.44ms, 10.1MB)
+# 테스트 7 〉	통과 (0.36ms, 10.1MB)
+# 테스트 8 〉	통과 (0.08ms, 10.2MB)
+# 테스트 9 〉	통과 (0.06ms, 10.3MB)
+# 테스트 10 〉	통과 (0.30ms, 10.2MB)
+# 테스트 11 〉	통과 (0.19ms, 10.1MB)
+# 테스트 12 〉	통과 (0.69ms, 10.1MB)
+# 테스트 13 〉	통과 (0.36ms, 10.1MB)
+# 테스트 14 〉	통과 (0.02ms, 10.1MB)
+# 테스트 15 〉	통과 (0.49ms, 10.1MB)
+# 테스트 16 〉	통과 (0.00ms, 10.1MB)
 #
 # 효율성  테스트
-#
-# 테스트 1 〉	통과 (185.94ms, 16.2MB)
-# 테스트 2 〉	통과 (397.80ms, 22MB)
-# 테스트 3 〉	통과 (1828.42ms, 49.7MB)
-# 테스트 4 〉	통과 (143.68ms, 15MB)
-# 테스트 5 〉	통과 (1644.89ms, 51.9MB)
+# 테스트 1 〉	통과 (181.41ms, 16.3MB)
+# 테스트 2 〉	통과 (350.12ms, 21.9MB)
+# 테스트 3 〉	통과 (1870.59ms, 49.7MB)
+# 테스트 4 〉	통과 (143.50ms, 14.9MB)
+# 테스트 5 〉	통과 (1879.71ms, 51.8MB)
 #
 # 채점 결과
 #

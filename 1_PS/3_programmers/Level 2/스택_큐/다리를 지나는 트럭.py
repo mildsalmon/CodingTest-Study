efec3dc04bf53b1a77d98ee3e8e0c9401ec6ab8d
mildsalmon_local_pc <mildsalmon@gmail.com>
2022-03-25 10:07:45 +0900

    :zap:[Edit] 다리를 지나는 트럭 - 3차 시도 (성공 - 속도 개선)
    sum()은 O(n)이라 이 부분을 미리 계산해둔 값으로 대체하였다.

--- 1_PS/3_programmers/Level 2/스택_큐/다리를 지나는 트럭.py
+++ 1_PS/3_programmers/Level 2/스택_큐/다리를 지나는 트럭.py
@@ -12,13 +12,16 @@ Email   : mildsalmon@gamil.com
 def solution(bridge_length, weight, truck_weights):
     answer = 0
     bridge = [0 for _ in range(bridge_length)]
+    now_weight = 0
 
     while bridge:
-        bridge.pop(0)
+        now_weight -= bridge.pop(0)
 
         if truck_weights:
-            if sum(bridge) + truck_weights[0] <= weight:
-                bridge.append(truck_weights.pop(0))
+            if now_weight + truck_weights[0] <= weight:
+                temp = truck_weights.pop(0)
+                now_weight += temp
+                bridge.append(temp)
             else:
                 bridge.append(0)
 
@@ -31,20 +34,20 @@ solution(100,100,[10])
 solution(100,100,[10,10,10,10,10,10,10,10,10,10])
 
 # 정확성  테스트
-# 테스트 1 〉	통과 (12.51ms, 10.3MB)
-# 테스트 2 〉	통과 (1640.78ms, 10.1MB)
-# 테스트 3 〉	통과 (0.03ms, 10.2MB)
-# 테스트 4 〉	통과 (370.69ms, 10.3MB)
-# 테스트 5 〉	실패 (시간 초과)
-# 테스트 6 〉	통과 (1660.62ms, 10.4MB)
-# 테스트 7 〉	통과 (5.76ms, 10.4MB)
-# 테스트 8 〉	통과 (0.39ms, 10.3MB)
-# 테스트 9 〉	통과 (5.34ms, 10.2MB)
-# 테스트 10 〉	통과 (0.48ms, 10.2MB)
-# 테스트 11 〉	통과 (0.01ms, 10.3MB)
-# 테스트 12 〉	통과 (0.27ms, 10.1MB)
-# 테스트 13 〉	통과 (2.95ms, 10.1MB)
-# 테스트 14 〉	통과 (0.02ms, 10.3MB)
+# 테스트 1 〉	통과 (0.64ms, 10.3MB)
+# 테스트 2 〉	통과 (49.64ms, 10.2MB)
+# 테스트 3 〉	통과 (0.01ms, 10.2MB)
+# 테스트 4 〉	통과 (8.90ms, 10.3MB)
+# 테스트 5 〉	통과 (295.82ms, 10.3MB)
+# 테스트 6 〉	통과 (39.07ms, 10.3MB)
+# 테스트 7 〉	통과 (0.44ms, 10.4MB)
+# 테스트 8 〉	통과 (0.08ms, 10.2MB)
+# 테스트 9 〉	통과 (2.10ms, 10.4MB)
+# 테스트 10 〉	통과 (0.10ms, 10.2MB)
+# 테스트 11 〉	통과 (0.01ms, 10.2MB)
+# 테스트 12 〉	통과 (0.17ms, 10.2MB)
+# 테스트 13 〉	통과 (0.60ms, 10.1MB)
+# 테스트 14 〉	통과 (0.02ms, 10.1MB)
 #
 # 채점 결과
 # 정확성: 92.9

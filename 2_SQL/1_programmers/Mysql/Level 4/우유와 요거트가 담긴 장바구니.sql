bf0d4266efdb00df8816fad4c4ca18c2f82830d2
mildsalmon_local_pc <mildsalmon@gmail.com>
2022-03-25 11:42:30 +0900

    :zap:[Edit] 우유와 요거트가 담긴 장바구니 - 2차 시도 (성공 - JOIN)

--- 2_SQL/1_programmers/Mysql/Level 4/우유와 요거트가 담긴 장바구니.sql
+++ 2_SQL/1_programmers/Mysql/Level 4/우유와 요거트가 담긴 장바구니.sql
@@ -2,18 +2,13 @@
 Date    : 2022.03.25
 Update  : 2022.03.25
 Source  : 우유와 요거트가 담긴 장바구니.sql
-Purpose : 서브 쿼리 / MINUS /
+Purpose : 서브 쿼리 / MINUS / JOIN
 url     : https://programmers.co.kr/learn/courses/30/lessons/59042?language=mysql
 Author  : 김학진 (mildsalmon)
 Email   : mildsalmon@gamil.com
 */
 
-SELECT CART_ID
-FROM CART_PRODUCTS
-WHERE NAME = 'Milk'
-    AND CART_ID IN (
-        SELECT CART_ID
-        FROM CART_PRODUCTS
-        WHERE NAME = 'Yogurt'
-    )
+SELECT A.CART_ID
+FROM CART_PRODUCTS A JOIN CART_PRODUCTS B ON (A.CART_ID = B.CART_ID)
+WHERE A.NAME = 'Milk' AND B.NAME = 'Yogurt'
 ;

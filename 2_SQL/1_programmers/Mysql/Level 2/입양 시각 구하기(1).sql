67249f26bea400e139837c9c29c0633c4e699540
mildsalmon_local_pc <mildsalmon@gmail.com>
2022-03-27 10:54:19 +0900

    :zap:[Edit] (복습) 입양 시각 구하기(1) - 1차 시도 (성공)

--- 2_SQL/1_programmers/Mysql/Level 2/입양 시각 구하기(1).sql
+++ 2_SQL/1_programmers/Mysql/Level 2/입양 시각 구하기(1).sql
@@ -1,6 +1,6 @@
 /*
 Date    : 2022.03.22
-Update  : 2022.03.22
+Update  : 2022.03.27
 Source  : 입양 시각 구하기(1).sql
 Purpose : GROUP BY / CASTING / HAVING
 url     : https://programmers.co.kr/learn/courses/30/lessons/59412?language=mysql
@@ -8,10 +8,11 @@ Author  : 김학진 (mildsalmon)
 Email   : mildsalmon@gamil.com
 */
 
-SELECT HOUR(DATETIME) AS DATE
+SELECT HOUR(DATETIME)
      , COUNT(1)
 FROM ANIMAL_OUTS
+WHERE HOUR(DATETIME) >= 9
+     AND HOUR(DATETIME) < 20
 GROUP BY 1
-HAVING DATE >= 9 AND DATE < 20
 ORDER BY 1
 ;

e3018cbfe15d15af47762adf5d8a9141e088e4ef
mildsalmon_local_pc <mildsalmon@gmail.com>
2022-03-27 11:05:14 +0900

    :zap:[Edit] (복습) 입양 시각 구하기(2) - 1차 시도 (실패 후 성공)

--- 2_SQL/1_programmers/Mysql/Level 4/입양 시각 구하기(2).sql
+++ 2_SQL/1_programmers/Mysql/Level 4/입양 시각 구하기(2).sql
@@ -1,6 +1,6 @@
 /*
 Date    : 2022.03.22
-Update  : 2022.03.22
+Update  : 2022.03.27
 Source  : 입양 시각 구하기(2).sql
 Purpose : GROUP BY / CASTING / HAVING / SET(사용자 변수)
 url     : https://programmers.co.kr/learn/courses/30/lessons/59413?language=mysql
@@ -10,11 +10,16 @@ Email   : mildsalmon@gamil.com
 
 set @hour := -1;
 
-SELECT @hour := @hour + 1
-     , (SELECT COUNT(*)
-        FROM ANIMAL_OUTS
-        WHERE @hour = HOUR(DATETIME))
-FROM ANIMAL_OUTS
-WHERE @hour <= 22
-ORDER BY 1
+SELECT A.hour
+     , IFNULL(B.cnt, 0)
+FROM (
+    SELECT @hour := @hour + 1 AS hour
+    FROM ANIMAL_OUTS
+    WHERE @hour < 23
+) A LEFT JOIN (
+    SELECT HOUR(DATETIME) AS hour
+         , COUNT(1) AS cnt
+    FROM ANIMAL_OUTS
+    GROUP BY 1
+) B ON (A.hour = B.hour)
 ;

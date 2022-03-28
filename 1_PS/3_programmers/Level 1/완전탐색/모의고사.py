2e1f7cbe0e55e24e4e2da3fbc5f277c9d9aaa6f1
mildsalmon_local_pc <mildsalmon@gmail.com>
2022-03-28 09:46:13 +0900

    :zap:[Edit] 모의고사 - 3차 시도 (성공)
    사실 출제자는 이런 코드를 원한걸지도 모르겠다.

--- 1_PS/3_programmers/Level 1/완전탐색/모의고사.py
+++ 1_PS/3_programmers/Level 1/완전탐색/모의고사.py
@@ -7,74 +7,48 @@ url     : https://programmers.co.kr/learn/courses/30/lessons/42840?language=pyth
 Author  : 김학진 (mildsalmon)
 Email   : mildsalmon@gamil.com
 """
-import sys
-
-sys.setrecursionlimit(10 ** 5)
-
-
-def search(depth, cnt, people, correct, i):
-    global answer
-
-    if depth == len(answer):
-        cnt.append([correct, i])
-        return
-
-    if answer[depth] == people[depth]:
-        search(depth + 1, cnt, people, correct + 1, i)
-    else:
-        search(depth + 1, cnt, people, correct, i)
-
-
-def high_score(cnt):
-    high_score = []
-
-    high_score.append(cnt[0][1])
-    _max = cnt[0][0]
-
-    for i in range(1, len(cnt)):
-        if cnt[i][0] == _max:
-            high_score.append(cnt[i][1])
-        else:
-            break
-    high_score.sort()
-
-    return high_score
 
 
 def solution(answers):
-    global answer
+    answer = []
+    people = [[1, 2, 3, 4, 5],
+              [2, 1, 2, 3, 2, 4, 2, 5],
+              [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]]
+    cnt = [0, 0, 0]
 
-    answer = answers[:]
+    for i, value in enumerate(answers):
+        if people[0][i % len(people[0])] == value:
+            cnt[0] += 1
+        if people[1][i % len(people[1])] == value:
+            cnt[1] += 1
+        if people[2][i % len(people[2])] == value:
+            cnt[2] += 1
 
-    people = []
-    people.append(list(range(1, 6)) * (10000 // 5))
-    people.append([2, 1, 2, 3, 2, 4, 2, 5] * (10000 // 8))
-    people.append([3, 3, 1, 1, 2, 2, 4, 4, 5, 5] * (10000 // 10))
+    _max = max(cnt)
 
-    cnt = []
-    for i in range(3):
-        search(0, cnt, people[i], 0, i + 1)
+    for i, value in enumerate(cnt):
+        if _max == value:
+            answer.append(i + 1)
 
-    cnt.sort(reverse=True)
-    people = high_score(cnt)
+    answer.sort()
 
-    return people
+    return answer
 
 # 정확성  테스트
-# 테스트 1 〉	통과 (0.18ms, 10.3MB)
-# 테스트 2 〉	통과 (0.21ms, 10.3MB)
-# 테스트 3 〉	통과 (0.22ms, 10.3MB)
-# 테스트 4 〉	통과 (0.22ms, 10.5MB)
-# 테스트 5 〉	통과 (0.21ms, 10.4MB)
-# 테스트 6 〉	통과 (0.38ms, 10.5MB)
-# 테스트 7 〉	통과 (8.44ms, 14.1MB)
-# 테스트 8 〉	통과 (2.45ms, 11.4MB)
-# 테스트 9 〉	통과 (16.16ms, 17.9MB)
-# 테스트 10 〉	통과 (6.91ms, 13.8MB)
-# 테스트 11 〉	통과 (15.77ms, 18.2MB)
-# 테스트 12 〉	통과 (14.61ms, 17.5MB)
-# 테스트 13 〉	통과 (0.81ms, 10.6MB)
-# 테스트 14 〉	통과 (17.55ms, 18.9MB)
+# 테스트 1 〉	통과 (0.01ms, 10.2MB)
+# 테스트 2 〉	통과 (0.01ms, 10.2MB)
+# 테스트 3 〉	통과 (0.01ms, 10.2MB)
+# 테스트 4 〉	통과 (0.01ms, 10.2MB)
+# 테스트 5 〉	통과 (0.03ms, 10.4MB)
+# 테스트 6 〉	통과 (0.05ms, 10.2MB)
+# 테스트 7 〉	통과 (1.87ms, 10.3MB)
+# 테스트 8 〉	통과 (0.62ms, 10.2MB)
+# 테스트 9 〉	통과 (3.35ms, 10.2MB)
+# 테스트 10 〉	통과 (1.61ms, 10.3MB)
+# 테스트 11 〉	통과 (3.71ms, 10.5MB)
+# 테스트 12 〉	통과 (3.19ms, 10.3MB)
+# 테스트 13 〉	통과 (0.20ms, 10.2MB)
+# 테스트 14 〉	통과 (3.65ms, 10.4MB)
 #
 # 채점 결과
 # 정확성: 100.0

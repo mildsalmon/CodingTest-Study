05b5f7f4a9a6d7f21a188f81a22bd50a319387ce
mildsalmon_local_pc <mildsalmon@gmail.com>
2022-03-28 11:21:28 +0900

    :zap:[Edit] 소수 찾기 - 2차 시도 (성공 - 다른 사람 풀이 참고해서 개선)

--- 1_PS/3_programmers/Level 2/완전탐색/소수 찾기.py
+++ 1_PS/3_programmers/Level 2/완전탐색/소수 찾기.py
@@ -8,63 +8,45 @@ Author  : 김학진 (mildsalmon)
 Email   : mildsalmon@gamil.com
 """
 
-import math
+from itertools import permutations
 
 
-def is_prime(num):
-    if num <= 1:
-        return False
-
-    for i in range(2, int(math.sqrt(num)) + 1):
-        if num % i == 0:
+def is_prime(x):
+    for i in range(2, int(x ** 0.5 + 1)):
+        if x % i == 0:
             return False
     return True
 
 
-def make_number(depth, numbers, visited, result):
-    global nums
+def solution(numbers):
+    answer = 0
 
-    if depth == len(numbers):
-        nums.add(int(result))
-        return
+    numbers_set = set()
 
     for i in range(len(numbers)):
-        if visited[i]:
-            make_number(depth + 1, numbers, visited[:], result)
-            continue
-        visited[i] = True
-        make_number(depth + 1, numbers, visited[:], result + numbers[i])
-        visited[i] = False
+        numbers_set |= set(map(int, map(''.join, (permutations(numbers, i + 1)))))
 
+    numbers_set -= set(range(2))
 
-def solution(numbers):
-    global nums
-
-    answer = 0
-    nums = set()
-
-    visited = [False for _ in range(len(numbers))]
-    make_number(0, numbers, visited, '')
-
-    for num in nums:
+    for num in numbers_set:
         if is_prime(num):
             answer += 1
 
     return answer
 
 # 정확성  테스트
-# 테스트 1 〉	통과 (0.18ms, 10.4MB)
-# 테스트 2 〉	통과 (23.39ms, 10.5MB)
-# 테스트 3 〉	통과 (0.03ms, 10.5MB)
-# 테스트 4 〉	통과 (21.60ms, 10.4MB)
-# 테스트 5 〉	통과 (373.27ms, 10.3MB)
-# 테스트 6 〉	통과 (0.03ms, 10.4MB)
-# 테스트 7 〉	통과 (0.19ms, 10.4MB)
-# 테스트 8 〉	통과 (367.91ms, 10.5MB)
-# 테스트 9 〉	통과 (0.05ms, 10.3MB)
-# 테스트 10 〉	통과 (26.90ms, 10.4MB)
-# 테스트 11 〉	통과 (2.57ms, 10.4MB)
-# 테스트 12 〉	통과 (1.67ms, 10.4MB)
+# 테스트 1 〉	통과 (0.06ms, 10.3MB)
+# 테스트 2 〉	통과 (2.97ms, 10.3MB)
+# 테스트 3 〉	통과 (1.03ms, 10.3MB)
+# 테스트 4 〉	통과 (0.80ms, 10.2MB)
+# 테스트 5 〉	통과 (2.64ms, 10.2MB)
+# 테스트 6 〉	통과 (0.65ms, 10.3MB)
+# 테스트 7 〉	통과 (0.06ms, 10.2MB)
+# 테스트 8 〉	통과 (2.60ms, 10.3MB)
+# 테스트 9 〉	통과 (0.04ms, 10.3MB)
+# 테스트 10 〉	통과 (5.09ms, 10.3MB)
+# 테스트 11 〉	통과 (0.55ms, 10.2MB)
+# 테스트 12 〉	통과 (0.17ms, 10.3MB)
 #
 # 채점 결과
 # 정확성: 100.0

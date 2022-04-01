2db8daf45db3e7f8e765983ed73ab13c613d4007
mildsalmon_local_pc <mildsalmon@gmail.com>
2022-04-01 21:23:44 +0900

    :zap:[Edit] (복습) 타겟 넘버 - 2차 시도 (성공 - product(cartesian product))

--- 1_PS/3_programmers/Level 2/DFS-BFS/타겟 넘버.py
+++ 1_PS/3_programmers/Level 2/DFS-BFS/타겟 넘버.py
@@ -7,27 +7,15 @@ url     : https://programmers.co.kr/learn/courses/30/lessons/43165
 Author  : 김학진 (mildsalmon)
 Email   : mildsalmon@gamil.com
 """
-
-
-def dfs(depth, numbers, target):
-    global answer
-
-    if depth == len(numbers):
-        if target == sum(numbers):
-            answer += 1
-        return
-
-    temp = numbers[:]
-    temp[depth] = -temp[depth]
-    dfs(depth + 1, temp, target)
-    temp[depth] = -temp[depth]
-    dfs(depth + 1, temp, target)
+from itertools import product
 
 
 def solution(numbers, target):
-    global answer
     answer = 0
+    temp = [[x, -x] for x in numbers]
 
-    dfs(0, numbers, target)
+    for num in product(*temp):
+        if sum(num) == target:
+            answer += 1
 
     return answer

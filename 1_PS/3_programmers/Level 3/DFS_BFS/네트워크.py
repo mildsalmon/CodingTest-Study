2909f8ba216df7a41de0bb4193e37c167009a13c
mildsalmon_local_pc <mildsalmon@gmail.com>
2022-04-03 06:52:47 +0900

    :zap:[Edit] (복습) 네트워크 - 1차 시도 (성공)

--- 1_PS/3_programmers/Level 3/DFS_BFS/네트워크.py
+++ 1_PS/3_programmers/Level 3/DFS_BFS/네트워크.py
@@ -1,47 +1,53 @@
 """
 Date    : 2022.04.01
-Update  : 2022.04.01
+Update  : 2022.04.03
 Source  : 네트워크.py
 Purpose : dfs
 url     : https://programmers.co.kr/learn/courses/30/lessons/43162?language=python3
 Author  : 김학진 (mildsalmon)
 Email   : mildsalmon@gamil.com
 """
-def dfs(i, visited, computers):
-    visited[i] = True
 
-    for j in range(len(computers)):
-        if computers[i][j] == 1 and not visited[j]:
-            dfs(j, visited, computers)
+
+def search(depth, visited, computers):
+    visited[depth] = True
+
+    for i, computer in enumerate(computers[depth]):
+        if i == depth:
+            continue
+        if visited[i]:
+            continue
+        if computer == 0:
+            continue
+        search(i, visited, computers)
 
 
 def solution(n, computers):
-    global answer
-
     answer = 0
-    visited = [False for _ in range(n)]
+    visited = [False for _ in range(len(computers))]
 
-    for i in range(n):
-        if not visited[i]:
-            dfs(i, visited, computers)
-            answer += 1
+    for i in range(len(computers)):
+        if visited[i]:
+            continue
+        answer += 1
+        search(i, visited, computers)
 
     return answer
 
 # 정확성  테스트
 # 테스트 1 〉	통과 (0.01ms, 10.1MB)
-# 테스트 2 〉	통과 (0.01ms, 10.2MB)
-# 테스트 3 〉	통과 (0.03ms, 10.2MB)
-# 테스트 4 〉	통과 (0.03ms, 10MB)
-# 테스트 5 〉	통과 (0.01ms, 10.1MB)
-# 테스트 6 〉	통과 (0.15ms, 10.4MB)
-# 테스트 7 〉	통과 (0.03ms, 10.3MB)
-# 테스트 8 〉	통과 (0.10ms, 10.1MB)
+# 테스트 2 〉	통과 (0.01ms, 10.3MB)
+# 테스트 3 〉	통과 (0.04ms, 10.2MB)
+# 테스트 4 〉	통과 (0.03ms, 10.1MB)
+# 테스트 5 〉	통과 (0.01ms, 10.3MB)
+# 테스트 6 〉	통과 (0.15ms, 10.1MB)
+# 테스트 7 〉	통과 (0.02ms, 10.3MB)
+# 테스트 8 〉	통과 (0.11ms, 10.1MB)
 # 테스트 9 〉	통과 (0.07ms, 10.2MB)
-# 테스트 10 〉	통과 (0.07ms, 10.3MB)
-# 테스트 11 〉	통과 (1.00ms, 10.2MB)
-# 테스트 12 〉	통과 (0.41ms, 10MB)
-# 테스트 13 〉	통과 (0.20ms, 10.1MB)
+# 테스트 10 〉	통과 (0.07ms, 10.1MB)
+# 테스트 11 〉	통과 (0.52ms, 10.4MB)
+# 테스트 12 〉	통과 (0.43ms, 10.2MB)
+# 테스트 13 〉	통과 (0.22ms, 10.2MB)
 #
 # 채점 결과
 # 정확성: 100.0

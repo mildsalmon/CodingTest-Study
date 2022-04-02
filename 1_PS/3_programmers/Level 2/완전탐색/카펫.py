c51241b6a18bb3582c488c7c29b48980ce57295e
mildsalmon_local_pc <mildsalmon@gmail.com>
2022-04-03 06:13:13 +0900

    :zap:[Edit] (복습) 카펫 - 1차 시도 (성공 - filter 활용하기)

--- 1_PS/3_programmers/Level 2/완전탐색/카펫.py
+++ 1_PS/3_programmers/Level 2/완전탐색/카펫.py
@@ -7,43 +7,39 @@ url     : https://programmers.co.kr/learn/courses/30/lessons/42842
 Author  : 김학진 (mildsalmon)
 Email   : mildsalmon@gamil.com
 """
-def get_divisor(x):
-    divisor = []
-    for i in range(1, int(x ** 0.5 + 1)):
+
+
+def divide(x):
+    num = set()
+
+    for i in range(1, int(x ** 0.5) + 1):
         if x % i == 0:
-            divisor.append((x // i, i))
-
-    return divisor
-
-
-def check_edge(x, y):
-    return 2 * x + 2 * y - 4
+            num.add((x // i, i))
+    return num
 
 
 def solution(brown, yellow):
-    total = brown + yellow
+    answer = []
 
-    divisors = get_divisor(total)
+    carpets = divide(brown + yellow)
+    carpet = list(filter(lambda x: x if 2 * x[0] + 2 * x[1] - 4 == brown else None, carpets))
 
-    for x, y in divisors:
-        edge = check_edge(x, y)
-        if edge == brown:
-            return [x, y]
+    return carpet[0]
 
 # 정확성  테스트
 # 테스트 1 〉	통과 (0.02ms, 10.2MB)
 # 테스트 2 〉	통과 (0.02ms, 10.2MB)
 # 테스트 3 〉	통과 (0.06ms, 10.2MB)
 # 테스트 4 〉	통과 (0.02ms, 10.3MB)
-# 테스트 5 〉	통과 (0.03ms, 10.2MB)
-# 테스트 6 〉	통과 (0.05ms, 10.2MB)
-# 테스트 7 〉	통과 (0.08ms, 10.2MB)
+# 테스트 5 〉	통과 (0.02ms, 10.1MB)
+# 테스트 6 〉	통과 (0.06ms, 10.2MB)
+# 테스트 7 〉	통과 (0.09ms, 10.3MB)
 # 테스트 8 〉	통과 (0.07ms, 10.3MB)
-# 테스트 9 〉	통과 (0.08ms, 10.3MB)
-# 테스트 10 〉	통과 (0.07ms, 10.3MB)
-# 테스트 11 〉	통과 (0.01ms, 10.3MB)
-# 테스트 12 〉	통과 (0.01ms, 10.2MB)
-# 테스트 13 〉	통과 (0.01ms, 10.3MB)
+# 테스트 9 〉	통과 (0.08ms, 10.2MB)
+# 테스트 10 〉	통과 (0.08ms, 10.2MB)
+# 테스트 11 〉	통과 (0.02ms, 10.3MB)
+# 테스트 12 〉	통과 (0.01ms, 10.3MB)
+# 테스트 13 〉	통과 (0.02ms, 10.3MB)
 #
 # 채점 결과
 # 정확성: 100.0

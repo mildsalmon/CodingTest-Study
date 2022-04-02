e086e9e6976746cbae5cd8fb5da13f6c919665e9
mildsalmon_local_pc <mildsalmon@gmail.com>
2022-04-03 05:04:10 +0900

    :zap:[Edit] (복습) 소수찾기 - 1차 시도 (성공)

--- 1_PS/3_programmers/Level 2/완전탐색/소수 찾기.py
+++ 1_PS/3_programmers/Level 2/완전탐색/소수 찾기.py
@@ -7,12 +7,14 @@ url     : https://programmers.co.kr/learn/courses/30/lessons/42839?language=pyth
 Author  : 김학진 (mildsalmon)
 Email   : mildsalmon@gamil.com
 """
-
 from itertools import permutations
 
 
 def is_prime(x):
-    for i in range(2, int(x ** 0.5 + 1)):
+    if x <= 1:
+        return False
+
+    for i in range(2, int(x ** 0.5) + 1):
         if x % i == 0:
             return False
     return True
@@ -20,33 +22,33 @@ def is_prime(x):
 
 def solution(numbers):
     answer = 0
+    numbers = list(numbers)
+    per = []
 
-    numbers_set = set()
+    for i in range(1, len(numbers) + 1):
+        per.extend(list(permutations(numbers, i)))
 
-    for i in range(len(numbers)):
-        numbers_set |= set(map(int, map(''.join, (permutations(numbers, i + 1)))))
+    per = set(map(lambda x: int(''.join(x)), per))
 
-    numbers_set -= set(range(2))
-
-    for num in numbers_set:
-        if is_prime(num):
+    for value in per:
+        if is_prime(value):
             answer += 1
 
     return answer
 
 # 정확성  테스트
-# 테스트 1 〉	통과 (0.06ms, 10.3MB)
-# 테스트 2 〉	통과 (2.97ms, 10.3MB)
-# 테스트 3 〉	통과 (1.03ms, 10.3MB)
-# 테스트 4 〉	통과 (0.80ms, 10.2MB)
-# 테스트 5 〉	통과 (2.64ms, 10.2MB)
-# 테스트 6 〉	통과 (0.65ms, 10.3MB)
-# 테스트 7 〉	통과 (0.06ms, 10.2MB)
-# 테스트 8 〉	통과 (2.60ms, 10.3MB)
-# 테스트 9 〉	통과 (0.04ms, 10.3MB)
-# 테스트 10 〉	통과 (5.09ms, 10.3MB)
-# 테스트 11 〉	통과 (0.55ms, 10.2MB)
-# 테스트 12 〉	통과 (0.17ms, 10.3MB)
+# 테스트 1 〉	통과 (0.06ms, 10.4MB)
+# 테스트 2 〉	통과 (2.60ms, 10.4MB)
+# 테스트 3 〉	통과 (0.03ms, 10.3MB)
+# 테스트 4 〉	통과 (1.17ms, 10.6MB)
+# 테스트 5 〉	통과 (5.76ms, 11.4MB)
+# 테스트 6 〉	통과 (0.04ms, 10.2MB)
+# 테스트 7 〉	통과 (0.07ms, 10.4MB)
+# 테스트 8 〉	통과 (5.29ms, 11.4MB)
+# 테스트 9 〉	통과 (0.04ms, 10.2MB)
+# 테스트 10 〉	통과 (5.48ms, 10.4MB)
+# 테스트 11 〉	통과 (0.57ms, 10.3MB)
+# 테스트 12 〉	통과 (0.21ms, 10.1MB)
 #
 # 채점 결과
 # 정확성: 100.0

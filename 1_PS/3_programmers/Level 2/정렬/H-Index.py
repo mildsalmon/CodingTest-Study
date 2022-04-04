5aee1e34f00257d7be5602e3492abd28fcd5cc2f
mildsalmon_local_pc <mildsalmon@gmail.com>
2022-04-04 11:50:33 +0900

    :zap:[Edit] H-Index - 2차 시도 (성공 - 다른 사람의 풀이)

--- 1_PS/3_programmers/Level 2/정렬/H-Index.py
+++ 1_PS/3_programmers/Level 2/정렬/H-Index.py
@@ -13,19 +13,14 @@ def solution(citations):
     answer = 0
     citations.sort()
 
-    for i in range(max(citations)):
-        quotation = [0, 0]
-        for j, citation in enumerate(citations):
-            if citation >= i:
-                quotation[0] = len(citations[j:])
-                quotation[1] = len(citations[:j])
-                break
-        if quotation[0] >= i and quotation[1] <= i:
-            answer = i
+    for i, citation in enumerate(citations):
+        if citation >= len(citations) - i:
+            return len(citations) - i
 
     return answer
 
-solution([3,0,6,1,5])
-solution([10,8,5,4,3]) # 4
-solution([25,8,5,3,3]) # 3
-solution([ 9, 7, 6, 2,1]) # 3
+print(solution([3,0,6,1,5]))
+print(solution([10,8,5,4,3])) # 4
+print(solution([25,8,5,3,3])) # 3
+print(solution([ 9, 7, 6, 2,1])) # 3
+print(solution([6,6,6,6,6,1])) # 5

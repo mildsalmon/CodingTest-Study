5a73795b64dfd5b6e6dee9555cf2e88848aa4714
mildsalmon_local_pc <mildsalmon@gmail.com>
2022-04-05 13:00:33 +0900

    :tada:[Add] Weather Observation Station 1 - 1차 시도 (성공)

---
+++ 2_SQL/2_HackerRank/Mysql/1. Easy/Weather Observation Station 1.sql
@@ -0,0 +1,13 @@
+/*
+Date    : 2022.04.05
+Update  : 2022.04.05
+Source  : Weather Observation Station 1.sql
+Purpose : SELECT
+url     : https://www.hackerrank.com/challenges/weather-observation-station-1/problem?isFullScreen=true
+Author  : 김학진 (mildsalmon)
+Email   : mildsalmon@gamil.com
+*/
+
+SELECT city, state
+FROM station
+;

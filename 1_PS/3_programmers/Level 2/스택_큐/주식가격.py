474ada851726a2276f61a0db75a8c6a4d98e3585
mildsalmon_local_pc <mildsalmon@gmail.com>
2022-04-05 10:04:36 +0900

    :zap:[Edit] 주식가격 - 4차 시도 (실패)
    3. 첫 번째 for문의 enumerate를 range로 변경
      => enumerate(prices) -> range(len(prices))
    
    시간 초과

--- 1_PS/3_programmers/Level 2/스택_큐/주식가격.py
+++ 1_PS/3_programmers/Level 2/스택_큐/주식가격.py
@@ -12,27 +12,27 @@ Email   : mildsalmon@gamil.com
 def solution(prices):
     answer = []
 
-    for i, price in enumerate(prices):
+    for i in range(len(prices)):
         sec = 0
-        for j in prices[i + 1:]:
+        for j, next_price in enumerate(prices[i + 1:]):
             sec += 1
-            if price > j:
+            if prices[i] > next_price:
                 break
         answer.append(sec)
 
     return answer
 
 # 정확성  테스트
-# 테스트 1 〉	통과 (0.00ms, 10.2MB)
-# 테스트 2 〉	통과 (0.04ms, 10.3MB)
-# 테스트 3 〉	통과 (0.72ms, 10.4MB)
-# 테스트 4 〉	통과 (0.88ms, 10.2MB)
-# 테스트 5 〉	통과 (1.88ms, 10.2MB)
-# 테스트 6 〉	통과 (0.02ms, 10.3MB)
-# 테스트 7 〉	통과 (0.38ms, 10.3MB)
-# 테스트 8 〉	통과 (0.51ms, 10.1MB)
-# 테스트 9 〉	통과 (0.02ms, 10.3MB)
-# 테스트 10 〉	통과 (1.17ms, 10.2MB)
+# 테스트 1 〉	통과 (0.01ms, 10.3MB)
+# 테스트 2 〉	통과 (0.06ms, 10.3MB)
+# 테스트 3 〉	통과 (0.89ms, 10.2MB)
+# 테스트 4 〉	통과 (1.12ms, 10.2MB)
+# 테스트 5 〉	통과 (1.47ms, 10.3MB)
+# 테스트 6 〉	통과 (0.03ms, 10.2MB)
+# 테스트 7 〉	통과 (0.52ms, 10.2MB)
+# 테스트 8 〉	통과 (0.66ms, 10.2MB)
+# 테스트 9 〉	통과 (0.03ms, 10.3MB)
+# 테스트 10 〉	통과 (1.54ms, 10.4MB)
 #
 # 효율성  테스트
 # 테스트 1 〉	실패 (시간 초과)

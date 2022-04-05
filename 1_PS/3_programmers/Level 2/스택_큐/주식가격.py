df295664b2abf7883a0d4abe0a1e085b2bdd456a
mildsalmon_local_pc <mildsalmon@gmail.com>
2022-04-05 10:14:56 +0900

    :tada:[Add] 주식가격 - 5차 시도 (성공 - stack)

--- 1_PS/3_programmers/Level 2/스택_큐/주식가격.py
+++ 1_PS/3_programmers/Level 2/스택_큐/주식가격.py
@@ -10,38 +10,37 @@ Email   : mildsalmon@gamil.com
 
 
 def solution(prices):
-    answer = []
+    answer = [i for i in range(len(prices) - 1, -1, -1)]
+    stack = []
 
-    for i in range(len(prices)):
-        sec = 0
-        for j, next_price in enumerate(prices[i + 1:]):
-            sec += 1
-            if prices[i] > next_price:
-                break
-        answer.append(sec)
+    for i, price in enumerate(prices):
+        while stack and prices[stack[-1]] > price:
+            j = stack.pop()
+            answer[j] = i - j
+        stack.append(i)
 
     return answer
 
 # 정확성  테스트
-# 테스트 1 〉	통과 (0.01ms, 10.3MB)
-# 테스트 2 〉	통과 (0.06ms, 10.3MB)
-# 테스트 3 〉	통과 (0.89ms, 10.2MB)
-# 테스트 4 〉	통과 (1.12ms, 10.2MB)
-# 테스트 5 〉	통과 (1.47ms, 10.3MB)
-# 테스트 6 〉	통과 (0.03ms, 10.2MB)
-# 테스트 7 〉	통과 (0.52ms, 10.2MB)
-# 테스트 8 〉	통과 (0.66ms, 10.2MB)
-# 테스트 9 〉	통과 (0.03ms, 10.3MB)
-# 테스트 10 〉	통과 (1.54ms, 10.4MB)
+# 테스트 1 〉	통과 (0.00ms, 10.2MB)
+# 테스트 2 〉	통과 (0.03ms, 10.1MB)
+# 테스트 3 〉	통과 (0.21ms, 10.2MB)
+# 테스트 4 〉	통과 (0.25ms, 10.2MB)
+# 테스트 5 〉	통과 (0.29ms, 10.1MB)
+# 테스트 6 〉	통과 (0.02ms, 10.2MB)
+# 테스트 7 〉	통과 (0.15ms, 10.3MB)
+# 테스트 8 〉	통과 (0.18ms, 10.2MB)
+# 테스트 9 〉	통과 (0.02ms, 10.1MB)
+# 테스트 10 〉	통과 (0.30ms, 10.3MB)
 #
 # 효율성  테스트
-# 테스트 1 〉	실패 (시간 초과)
-# 테스트 2 〉	실패 (시간 초과)
-# 테스트 3 〉	실패 (시간 초과)
-# 테스트 4 〉	실패 (시간 초과)
-# 테스트 5 〉	실패 (시간 초과)
+# 테스트 1 〉	통과 (27.03ms, 19.3MB)
+# 테스트 2 〉	통과 (20.30ms, 17.9MB)
+# 테스트 3 〉	통과 (30.46ms, 20.1MB)
+# 테스트 4 〉	통과 (42.19ms, 18.7MB)
+# 테스트 5 〉	통과 (16.58ms, 17.2MB)
 #
 # 채점 결과
 # 정확성: 66.7
-# 효율성: 0.0
-# 합계: 66.7 / 100.0
+# 효율성: 33.3
+# 합계: 100.0 / 100.0

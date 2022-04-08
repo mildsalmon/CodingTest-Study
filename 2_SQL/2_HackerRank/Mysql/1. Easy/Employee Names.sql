1fea2d72607c5503d47cb902f447694468a4bfcd
mildsalmon_local_pc <mildsalmon@gmail.com>
2022-04-08 12:28:35 +0900

    :tada:[Add] Employee Names - 1차 시도 (성공)

---
+++ 2_SQL/2_HackerRank/Mysql/1. Easy/Employee Names.sql
@@ -0,0 +1,14 @@
+/*
+Date    : 2022.04.08
+Update  : 2022.04.08
+Source  : Employee Names.sql
+Purpose : SELECT
+url     : https://www.hackerrank.com/challenges/name-of-employees/problem?isFullScreen=true
+Author  : 김학진 (mildsalmon)
+Email   : mildsalmon@gamil.com
+*/
+
+SELECT name
+FROM employee
+ORDER BY 1 ASC
+;

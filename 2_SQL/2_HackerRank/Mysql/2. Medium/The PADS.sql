b0c25e94e2d7feedc336acec892918392f56aab5
mildsalmon_local_pc <mildsalmon@gmail.com>
2022-04-09 11:47:18 +0900

    :zap:[Edit] (복습) The PADS - 1차 시도 (성공)

--- 2_SQL/2_HackerRank/Mysql/2. Medium/The PADS.sql
+++ 2_SQL/2_HackerRank/Mysql/2. Medium/The PADS.sql
@@ -1,26 +1,18 @@
 /*
 Date    : 2022.04.08
-Update  : 2022.04.08
+Update  : 2022.04.09
 Source  : The PADS.sql
 Purpose : SELECT / CASE WHEN / CONCAT
 url     : https://www.hackerrank.com/challenges/the-pads/problem?isFullScreen=true
 Author  : 김학진 (mildsalmon)
 Email   : mildsalmon@gamil.com
 */
-
-SELECT CONCAT(name, CASE occupation
-                        WHEN 'Doctor' THEN '(D)'
-                        WHEN 'Actor' THEN '(A)'
-                        WHEN 'Singer' THEN '(S)'
-                        WHEN 'Professor' THEN '(P)'
-                    END
-         ) s
+SELECT CONCAT(name, '(', SUBSTR(occupation, 1, 1), ')')
 FROM occupations
-ORDER BY 1 ASC
+ORDER BY 1
 ;
-SELECT CONCAT('There are a total of ', COUNT(occupation),' ', LOWER(occupation), 's.') AS s
+SELECT CONCAT('There are a total of ', COUNT(name),' ', LOWER(occupation), 's.')
 FROM occupations
 GROUP BY occupation
-ORDER BY COUNT(occupation) ASC, occupation ASC
-
+ORDER BY 1
 ;

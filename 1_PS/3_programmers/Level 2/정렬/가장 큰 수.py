423b03f98279cb52da4b4c8e4806fa4585423e1d
mildsalmon_local_pc <mildsalmon@gmail.com>
2022-04-09 10:15:38 +0900

    :zap:[Edit] (복습) 가장 큰 수 - 1차 시도 (성공)

--- 1_PS/3_programmers/Level 2/정렬/가장 큰 수.py
+++ 1_PS/3_programmers/Level 2/정렬/가장 큰 수.py
@@ -1,6 +1,6 @@
 """
 Date    : 2022.04.04
-Update  : 2022.04.04
+Update  : 2022.04.09
 Source  : 가장 큰 수.py
 Purpose : 정렬, 아이디어
 url     : https://programmers.co.kr/learn/courses/30/lessons/42746
@@ -11,9 +11,9 @@ Email   : mildsalmon@gamil.com
 
 def solution(numbers):
     numbers = list(map(str, numbers))
-    numbers.sort(key=lambda x: x*3, reverse=True)
-    answer = str(int(''.join(numbers)))
-    return answer
+    numbers.sort(key=lambda x: x * 3, reverse=True)
+
+    return str(int(''.join(numbers)))
 
 print(solution([6, 10, 2, 0]))
 print(solution([0, 0, 0]))

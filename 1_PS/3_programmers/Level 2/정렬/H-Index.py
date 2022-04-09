f3862632198d243fc40b6c6f9db4c7e1f913cc30
mildsalmon_local_pc <mildsalmon@gmail.com>
2022-04-09 11:22:07 +0900

    :zap:[Edit] (복습) H-Index - 1차 시도 (성공)

--- 1_PS/3_programmers/Level 2/정렬/H-Index.py
+++ 1_PS/3_programmers/Level 2/정렬/H-Index.py
@@ -1,6 +1,6 @@
 """
 Date    : 2022.04.04
-Update  : 2022.04.04
+Update  : 2022.04.09
 Source  : H-Index.py
 Purpose : 정렬, 아이디어
 url     : https://programmers.co.kr/learn/courses/30/lessons/42747
@@ -11,11 +11,15 @@ Email   : mildsalmon@gamil.com
 
 def solution(citations):
     answer = 0
-    citations.sort()
 
-    for i, citation in enumerate(citations):
-        if citation >= len(citations) - i:
-            return len(citations) - i
+    citations.sort()
+    h_index = [i for i in range(1, len(citations) + 1)]
+
+    for a, b in zip(citations[::-1], h_index):
+        if a >= b:
+            answer = b
+        else:
+            break
 
     return answer
 
@@ -24,3 +28,6 @@ print(solution([10,8,5,4,3])) # 4
 print(solution([25,8,5,3,3])) # 3
 print(solution([ 9, 7, 6, 2,1])) # 3
 print(solution([6,6,6,6,6,1])) # 5
+print(solution([1])) # 1
+print(solution([0])) # 0
+print(solution([2])) # 1

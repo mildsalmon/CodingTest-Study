908f1047df227966a973dd6c1840ce1a959d5185
mildsalmon_local_pc <mildsalmon@gmail.com>
2022-04-11 08:44:51 +0900

    :tada:[Add] [1차] 비밀지도  - 1차 시도 (성공)

--- 1_PS/3_programmers/Level 1/2018 KAKAO BLIND RECRUITMENT/[1차] 비밀지도.py
+++ 1_PS/3_programmers/Level 1/2018 KAKAO BLIND RECRUITMENT/[1차] 비밀지도.py
@@ -9,27 +9,52 @@ Email   : mildsalmon@gamil.com
 """
 
 
-def solution(n, arr1, arr2):
+def convert_binary(result, arr, n):
+    for num in arr:
+        binary = format(num, '0b')
+
+        if len(binary) < n:
+            binary = '0' * (n - len(binary)) + binary
+
+        result.append(binary)
+
+
+def concat_map(arr1, arr2, n):
     answer = []
 
     for a, b in zip(arr1, arr2):
-        temp = bin(a | b)[2:]
-        temp = temp.rjust(n, '0')
-        temp = temp.replace('1', '#')
-        temp = temp.replace('0', ' ')
+        temp = ''
+
+        for i in range(n):
+            if a[i] == '0' and b[i] == '0':
+                temp += ' '
+            else:
+                temp += '#'
         answer.append(temp)
 
     return answer
 
+
+def solution(n, arr1, arr2):
+    arr1_bi = []
+    arr2_bi = []
+
+    convert_binary(arr1_bi, arr1, n)
+    convert_binary(arr2_bi, arr2, n)
+
+    answer = concat_map(arr1_bi, arr2_bi, n)
+
+    return answer
+
 # 정확성  테스트
-# 테스트 1 〉	통과 (0.01ms, 10.3MB)
-# 테스트 2 〉	통과 (0.01ms, 10.1MB)
-# 테스트 3 〉	통과 (0.01ms, 10.3MB)
-# 테스트 4 〉	통과 (0.01ms, 10.2MB)
-# 테스트 5 〉	통과 (0.01ms, 10.1MB)
-# 테스트 6 〉	통과 (0.01ms, 10.1MB)
-# 테스트 7 〉	통과 (0.01ms, 10.1MB)
-# 테스트 8 〉	통과 (0.01ms, 10.1MB)
+# 테스트 1 〉	통과 (0.02ms, 10.2MB)
+# 테스트 2 〉	통과 (0.04ms, 10.2MB)
+# 테스트 3 〉	통과 (0.01ms, 10.4MB)
+# 테스트 4 〉	통과 (0.03ms, 10.3MB)
+# 테스트 5 〉	통과 (0.02ms, 10.2MB)
+# 테스트 6 〉	통과 (0.03ms, 10.2MB)
+# 테스트 7 〉	통과 (0.02ms, 10.1MB)
+# 테스트 8 〉	통과 (0.02ms, 10.2MB)
 #
 # 채점 결과
 # 정확성: 100.0

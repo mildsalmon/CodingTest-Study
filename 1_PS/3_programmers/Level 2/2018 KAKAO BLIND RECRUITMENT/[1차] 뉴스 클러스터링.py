535e972667b2091cca0bc40be9ca7adf0059d1eb
mildsalmon_local_pc <mildsalmon@gmail.com>
2022-04-14 22:47:33 +0900

    :zap:[Edit] [1차] 뉴스 클러스터링 - 2차 시도 (성공)

--- 1_PS/3_programmers/Level 2/2018 KAKAO BLIND RECRUITMENT/[1차] 뉴스 클러스터링.py
+++ 1_PS/3_programmers/Level 2/2018 KAKAO BLIND RECRUITMENT/[1차] 뉴스 클러스터링.py
@@ -25,11 +25,11 @@ def solution(str1, str2):
     s1_sub = str_set(str1)
     s2_sub = str_set(str2)
 
-    if not len(s1_sub) or not len(s2_sub):
-        return 1*65536
-
     intersection = set(s1_sub.keys()).intersection(set(s2_sub.keys()))
     union = set(s1_sub.keys()).union(set(s2_sub.keys()))
+
+    if not union:
+        return 1 * 65536
 
     intersection_cnt = 0
     union_cnt = 0
@@ -57,20 +57,20 @@ def solution(str1, str2):
     return int((intersection_cnt / union_cnt) * 65536)
 
 # 정확성  테스트
-# 테스트 1 〉	통과 (0.03ms, 10.4MB)
+# 테스트 1 〉	통과 (0.86ms, 10.4MB)
 # 테스트 2 〉	통과 (0.03ms, 10.3MB)
 # 테스트 3 〉	통과 (0.03ms, 10.3MB)
-# 테스트 4 〉	통과 (0.58ms, 10.3MB)
-# 테스트 5 〉	실패 (0.02ms, 10.3MB)
+# 테스트 4 〉	통과 (1.10ms, 10.4MB)
+# 테스트 5 〉	통과 (0.05ms, 10.3MB)
 # 테스트 6 〉	통과 (0.03ms, 10.3MB)
-# 테스트 7 〉	통과 (0.09ms, 10.5MB)
-# 테스트 8 〉	통과 (0.02ms, 10.3MB)
-# 테스트 9 〉	통과 (0.09ms, 10.2MB)
-# 테스트 10 〉	통과 (0.15ms, 10.3MB)
-# 테스트 11 〉	통과 (0.20ms, 10.3MB)
+# 테스트 7 〉	통과 (0.15ms, 10.3MB)
+# 테스트 8 〉	통과 (0.03ms, 10.3MB)
+# 테스트 9 〉	통과 (0.13ms, 10.4MB)
+# 테스트 10 〉	통과 (0.18ms, 10.3MB)
+# 테스트 11 〉	통과 (0.21ms, 10.4MB)
 # 테스트 12 〉	통과 (0.01ms, 10.3MB)
-# 테스트 13 〉	통과 (0.06ms, 10.2MB)
+# 테스트 13 〉	통과 (0.06ms, 10.4MB)
 #
 # 채점 결과
-# 정확성: 92.3
-# 합계: 92.3 / 100.0
+# 정확성: 100.0
+# 합계: 100.0 / 100.0
